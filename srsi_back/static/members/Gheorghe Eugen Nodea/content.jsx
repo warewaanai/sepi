61c3c207fe67e6e4780bf60d168de15a57142361
Serban Cercelescu <anai@Serbans-MacBook-Pro.local>
2021-02-11 20:53:48 +0000

    commit on 'main'

--- srsi_back/static/members/Gheorghe Eugen Nodea/content.jsx
+++ srsi_back/static/members/Gheorghe Eugen Nodea/content.jsx
@@ -1,13 +1,21 @@
 <ul>
-    <li> Team-leader al echipei de juniori a României la EJOI, edițiile 2017, 2019</li>
-    <li> Team/Deputy-leader al echipei de juniori a României la JBOI, edițiile 2013, 2014, 2015, 2016</li>
-    <li> Team leader al echipei de seniori a României la CEOI 2016</li>
-    <li> Team-leader al echipei de juniori/seniori a României la Turneul International de informatica de la Shumen, Bulgaria, edițiile 2013, 2018, 2020</li>
-    <li> Team-leader al echipei de juniori/seniori a României la RMI </li>
-    <li> Membru  in comitetului științific al Balcaniadei de informatica, ediția 2018</li>
-    <li> Membru in Comisia științifica a taberelor de pregătire si selecționare a loturilor naționale pentru competițiile internaționale, edițiile 2011 - prezent</li>
-    <li> Membru in Comisia Naționala de elaborare subiecte si evaluare a Olimpiadei Județene/Naționale de informatica, edițiile 2011-2020</li>
-    <li> Membru in Comisia Naționala de specialitate INFORMATICA 2016 - prezent</li>
-    <li> Coordonatorul Concursului Județean de informatica "Info-Oltenia", edițiile 2008, 2012, 2017</li>
-    <li> Coautor lucrări științifice și cărti din domeniul informaticii</li>
+    <li>Membru în Comisia Naționala de specialitate INFORMATICĂ a Ministerului Educației Naționale 2016-2021</li>
+
+    <li>Membru in Comitetului Științific al Balcaniadei de informatica, ediția 2018</li>
+
+    <li>Team Leader al echipei de juniori a României la European Junior Olympiad in Informatics (EJOI) edițiile 2017 (Bulgaria), 2019 (Slovenia)</li>
+    <li>Team/Deputy-Leader al echipei de juniori a României la Junior Balkan Olympiad in Informatics (JBOI) edițiile 2013 (Bulgaria), 2014 (Serbia), 2015 (Macedonia), 2016 (Macedonia)</li>
+    <li>Team Leader al echipei de seniori a României la CEOI 2016</li>
+    <li>Team Leader al echipei de juniori/seniori a României la Turneul International de informatica de la Shumen, Bulgaria, edițiile 2013, 2018, 2020</li>
+    <li>Team Leader al echipei de juniori/seniori a României la Romanian Master of Informatics</li>
+
+    <li>Membru în Comisiile de Selecție și Pregătire a Lotului Național de Informatică: Botoșani și Reșița 2012, Deva și Baia Mare 2013, Sovata și Vaslui 2014, Râmnicu Vâlcea și Drobeta Turnu Severin 2015, Deva și Măgurele 2016, Sovata și Alexandria 2017, Tulcea și Câmpulung Muscel 2018, Deva și Dorohoi 2019 </li>
+
+    <li>Membru in Comisia Națională de elaborare a subiectelor și evaluare a Olimpiadei Județene/Naționale de informatica, edițiile 2011-2020</li>
+    <li>Vicepreședinte subcomisii în cadrul Olimpiadei Naționale de Informatică</li>
+    <li>Vicepreședinte subcomisii in cadrul Olimpiadei Județene de Informatică</li>
+
+    <li>Coordonatorul Concursului Județean de informatica "Info-Oltenia", edițiile 2008, 2012, 2017</li>
+
+    <li>Coautor lucrări științifice și cărti din domeniul informaticii (7)</li>
 </ul>
