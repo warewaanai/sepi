e183cdca666584acdbd332f08f03a4068e395108
Bodo171 <pbppbp17@gmail.com>
2021-02-14 14:13:18 +0200

    Fix typo

--- srsi_back/static/members/Bogdan Pop/content.jsx
+++ srsi_back/static/members/Bogdan Pop/content.jsx
@@ -1,5 +1,5 @@
 <ul>
     <li>Medaliat cu Argint la Olimpiada Balcanică de Informatică (2019, Atena, Grecia) și Romanian Masters of Informatics (2018), bronz la Turneul de Informatică de la Șumen (2018) </li>
     <li>Medaliat cu Aur la Olimpiada Națională de Informatică (2018, 2019) și Argint (2017)</li>
-    <li>Mebru in comisia Lotului National de Informatica (2020), Romanian Master Of Informatics (2019, 2020) si a mai multor concursuri organizate pe platforma Infoarena.</li>
+    <li>Membru in comisia Lotului National de Informatica (2020), Romanian Master Of Informatics (2019, 2020) si a mai multor concursuri organizate pe platforma Infoarena.</li>
 </ul>
