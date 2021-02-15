2eb0ace532e2abb3456ce240ae93a26cf742159a
Bodo171 <pbppbp17@gmail.com>
2021-02-15 19:53:17 +0200

    Add OJI announcement

--- srsi_back/static/announcements/2/content.jsx
+++ srsi_back/static/announcements/2/content.jsx
@@ -1,1 +1,29 @@
-ttt
+<p>Dorim să venim în sprijinul elevilor pentru a participa la OSEPI dar în același timp să păstrăm gradul de corectitudine care se cere de la un concurs la un asemenea nivel.</p>
+
+<p>Am analizat cu atenție propunerile venite de la membri SEPI, de la colegii profesori din țară și nu în ultimul rând de la elevii care doresc să participe la prima etapă a OSEPI.</p>
+
+<p>În vederea bunei desfășurări a etapei județene a OSEPI 2021, venim cu următoarele precizări și amendamente la Regulament:</p>
+
+<p>	1. La secțiunea gimnaziu se vor desfășura patru probe pe parcursul a două zile, în intervale de timp distincte, câte una pentru fiecare dintre cele patru clase (a V-a, a VI-a, a VII-a și a VIII-a). </p>
+
+<p>     2. Similar, la secțiunea liceu se vor organiza trei probe pe parcursul a două zile (clasele a XI-a și a XII-a sunt singurele care vor da proba simultan deoarece au subiecte comune).</p>
+
+<p>     3. În condițiile precizate mai sus numărul de elevi care participă simultan la o probă face posibilă - acolo unde se dorește - organizarea probelor în scenariul Online instituțional. Se poate decide organizarea de centre la nivel de județ, la nivel de localitate sau la nivel de școală. Avem deja semnale că în multe locuri probele se pot desfășura în școli.</p>
+
+<p>     4. Școlile care vor organiza centre de concurs vor trimite Societății <b>lista nominală a elevilor prezenți în școală la probă. Se va anexa repartizarea elevilor pe săli, numele supraveghetorilor din fiecare sală și numele supraveghetorului de pe hol. </b>
+	<br /> Listele solicitate pentru organizarea probelor în scenariul <i>Online instituțional</i> pot fi trimise organizatorilor OSEPI și după încheierea etapei de înscriere.
+	<br /> În cazul elevilor care nu vor fi nominalizați pe o astfel de listă se subînțelege participarea acestora în scenariul <i>Online individual</i>.</p>
+
+<p>     5. În cazul organizării în scenariul <i> Online instituțional </i> <b>este necesară o filmare de ambient, audio-video pentru sala de clasă.</b> </p>
+
+<p>     6. Chiar și în cazul organizării de centre unde proba se susține după scenariul <i>Online instituțional</i>, elevii pot opta să susțină proba în scenariul <i>Online individual</i> (pot exista și motive obiective cum ar fi izolarea la domiciliu).</p>
+
+<p>     7. Ținând cont de numărul mare de solicitări primite și de analiza din cadrul grupului de lucru pentru organizarea OSEPI, am decis că elevii care vor susține proba de acasă (în scenariul Online individual) au obligația de a realiza două filmări audio/video (și nu 3 cum era anunțat inițial), cuprinzând:
+<br />(a) încăperea în care concurentul își desfășoară proba;
+<br />(b) activitatea de pe calculator (captând imaginea de pe ecran pe toată durata probei; recomandăm utilizarea softului OBS Studio).</p>
+
+<p> 	8. Reamintim că termenul limită pentru trimiterea formularelor de înscriere de către școli este <b>vineri, 19 februarie, ora 24.</b> </p>
+
+<p> 	9. Atenție la corectitudinea datelor din formularul de înscriere. Este foarte important ca adresa de email a elevului să fie corectă, deoarece concurenții vor primi prin email datele de autentificare la serverul de concurs.</p>
+
+<p>     10. Vom reveni cu alte detalii și precizări referitoare la organizarea probelor după încheierea perioadei de înscriere.</p>
