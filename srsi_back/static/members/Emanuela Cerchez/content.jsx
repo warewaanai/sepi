68a3a5e10f2d20b268b7619c29007e90fc798ed7
Bodo171 <pbppbp17@gmail.com>
2021-02-18 16:29:18 +0200

    Add member

---
+++ srsi_back/static/members/Emanuela Cerchez/content.jsx
@@ -0,0 +1,162 @@
+Prof. Colegiul Național ”Emil Racoviță” Iași
+
+Coordonator ştiinţific al pregătirii lotului naţional de informatică 2001-2008
+Membru al Comisiei Naționale a disciplinei Informatică 
+
+<ul>
+<li>
+Leader al echipei reprezantative a României:
+<ul>
+	<li> 2008 – Olimpiada Internaţională de Informatică, Egipt, Cairo, team leader</li>
+	<li> 2008 – Olimpiada de Informatică a Europei Centrale Germania Dresda, team leader </li>
+	<li> 2007 – Olimpiada Internaţională de Informatică, Croaţia, Zagreb, team leader</li>
+	<li> 2007 – Olimpiada de Informatică a Europei Centrale Cehia, Brno, team leader </li>
+	<li> 2006 – Olimpiada Internaţională de Informatică, Mexic, Merida, team leader</li>
+	<li> 2006 – Olimpiada de Informatică a Europei Centrale Croaţia, Vrsar, team leader </li>
+	<li> 2005 – Olimpiada Internaţională de Informatică, Polonia, Nowy Sacz, team leader</li>
+	<li> 2005 – Balcaniada de Informatică, Grecia, Rodos, team leader</li>
+	<li> 2004 – Olimpiada Internaţională de Informatică, Grecia, Atena, team leader</li>
+	<li> 2003 – Olimpiada Internaţională de Informatică, SUA, Wisconsin, team leader</li>
+	<li> 2003 – Balcaniada de Informatică, Iaşi, România, organizare, coordonator ştiinţific.</li>
+	<li> 2002 – Olimpiada Internaţională de Informatică, Corea, Yong-Yin, deputy leader</li>
+	<li> 2001 – Olimpiada Internaţională de Informatică, Finlanda, Tampere, deputy leader</li>
+	<li> 2000 – Balcaniada de Informatică, Macedonia, Ohrid, team leader </li>
+	<li> 2000 – Olimpiada de Informatică a Europei Centrale România, Cluj, team-leader</li> 
+</ul>
+</li>
+
+<li>
+Selectarea şi pregătirea lotului României
+<ul>
+	<li> 1996 – 2008 Pregătirea lotului României pentru diferite concursuri internaţionale, în diferite tabere şi concursuri de pregătire </li>
+	<li> 1996	– Tabăra de pregătire, Sibiu, august </li>
+	<li> 2000	– Tabăra concurs de pregătire a lotului naţional Oradea, august </li>
+	<li>
+        2001
+        <ul>	
+        <li> Tabăra de selecţie a lotului naţional restrâns, Bucureşti, iunie</li>
+		<li> Tabăra de pregătire a lotului naţional, Iaşi</li>
+        </ul>
+    </li>
+
+	<li>
+        2002
+        <ul>
+            <li>Tabăra de selecţie a lotului naţional, Focşani, mai</li>
+	        <li>Tabăra de selecţie a lotului naţional pentru IOI, Bucureşti, iulie</li>
+        </ul>	
+    </li>
+
+    <li>
+	    2003
+        <ul>
+            <li>Tabăra de pregătire şi selecţie a lotului naţional Slatina, mai</li>
+	        <li>Tabăra de pregătire şi selecţie a lotului naţional Focşani, iunie</li>
+        </ul>	
+    </li>
+
+    <li>   
+	    2004
+        <ul>
+            <li> Tabăra de pregătire şi selecţie a lotului naţional Alba-Iulia, mai</li>
+	        <li> Tabăra de pregătire şi selecţie a lotului naţional Orăştie, iunie</li>
+        </ul>
+    </li>
+
+    <li>
+	2005 – Tabăra de pregătire şi selecţie a lotului naţional Sibiu, iunie
+    </li> 
+
+    <li>
+	    2006
+        <ul>
+            <li> Tabăra de pregătire şi selecţie a lotului naţional Ploieşti, mai</li>
+	        <li> Tabăra de pregătire şi selecţie a lotului naţional Alba-Iulia, iunie</li>
+        </ul>
+    </li>
+
+    <li>
+	    2007
+        <ul>
+            <li> Tabăra de pregătire şi selecţie a lotului naţional Suceava, mai</li>
+		    <li> Tabăra de pregătire şi selecţie a lotului naţional Alba-Iulia, iunie</li>
+        </ul>
+    </li>
+
+    <li>
+	    2008
+        <ul>
+        <li> Tabăra de pregătire şi selecţie a lotului naţional Piatra-Neamţ, mai</li>
+		<li> Tabăra de pregătire şi selecţie a lotului naţional Iaşi, iunie</li>
+        </ul>
+    </li>
+</ul>
+</li>
+
+
+<li>
+Olimpiade naţionale
+<ul>
+	1996 – 2011, 2013-2019
+	Membru în comisia naţională de propunere de probleme şi corectare
+	Vicepreşedinte comisie
+</ul>
+</li>
+
+<li>
+Olimpiade judeţene
+<ul>
+    <li>
+	1993-2001
+		Membru în comisia judeţeană de propunere de probleme şi corectare
+    </li>
+    <li>
+	    2002-2020
+        <ul>
+		<li> Membru în comisia judeţeană de corectare</li>
+		<li> Membru în comisia naţională de elaborare de subiecte</li>
+        </ul>
+    </li>
+</ul>
+</li>
+
+<li>
+Autor a peste 20 de publicații - Manuale și cărți de specialitate
+</li>
+
+<li>
+Iniţiator şi coordonator al proiectului de pregătire de performanţă în informatică a elevilor <a href='http://campion.edu.ro'>„.campion”</a> (2003 – 2012).
+<br /> 
+Proiectul se încadrează în programul "Centru Virtual de Excelenţă" iniţiat firmei SIVECO, în colaborare cu M.Ed.C.
+<br />
+În fiecare an participă la acest program de pregătire săptămânală peste 1500 de elevi. În perioada 2005-2007 programul a fost internaţional. 
+</li>
+
+<li>
+Coordonator disciplina Informatică la Centrul Regional Pentru Tineri Capabili de Performanţă Iaşi (2001-2021)
+</li>
+
+<li>
+Concursuri naţionale / regionale
+<ul>
+	<li> 2013 - STARTIT – concurs national organizat de Palatul Copiilor Suceava; membru în comisia ştiinţifică şi de evaluare </li>
+	<li> 2011 - F11 Competition – concurs naţional organizat de Facultatea de Informatică Iaşi; membru în comisia ştiinţifică </li>
+	<li> 2011 - Şcoala de Vară InfoFizica – organizată de Universitatea "Al. I. Cuza" Iaşi şi de Centrul de Excelenţă Iaşi;  membru în comisia ştiinţifică</li>
+	<li> 2000-2005, 2007-2010 - "Urmaşii lui Mosil" – concurs naţional de informatică organizat de Liceul de Informatică "Gr. Moisil" Iaşi, organizator, coordonator ştiinţific</li>
+	<li> 1996-2003 -  "Infoeducaţie" – concurs naţional de creativitate software, membru în comisia de evaluare a lucrărilor la nivel judeţean şi naţional</li>
+	<li> 1999, 2001 - Concursul naţional de soft "Gr. Moisil" Lugoj, în comisia de elaborare subiecte şi  evaluare </li>
+    <li> 2000 -Concursul naţional de creativitate software "Noi Info", Timişoara, membru în comisia de jurizare </li>
+</ul>
+</li>
+
+<li>
+Membru sau coordonator al Comisiei de organizare la
+<ul>
+ 	<li> Balcanida de Informatică, Iaşi, România 2003 </li>
+	<li> Concursul Naţional de Informatică „Urmaşii lui Moisil” 2002- 2010 </li>
+	<li> Finala .campion (2004-2011) </li>
+	<li> Pregătirea lotului naţional de informatică, Iaşi iunie 2001 şi iunie 2008 </li>
+</ul>
+</li>
+
+</ul>
