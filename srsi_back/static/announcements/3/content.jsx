e929dd6a81669e71c074890b16a3783cee9f830e
Bodo171 <pbppbp17@gmail.com>
2021-02-22 11:09:04 +0200

    Add contestant list

---
+++ srsi_back/static/announcements/3/content.jsx
@@ -0,0 +1,5 @@
+<p> Conform calendarului de desfășurare OSEPI (care se găsește în regulament) afișăm <a target="_blank" href={`${window.HOST}/static/regulament.pdf`}> lista elevilor înscriși</a>. </p>
+
+<p> Etapa verificării suplimentare, în județe, a corectitudinii datelor a fost deja demarată și se va desfășura în această săptămână.</p>
+
+<p> Pentru situațiile speciale, în care nu s-a reușit deja înscrierea, vă rugăm să ne contactați la adresa de email inscriere.scoli@sepi.ro pana vineri 26 februarie 2021, ora 24.</p>
