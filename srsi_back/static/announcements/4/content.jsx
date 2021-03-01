ed3e4b1ba67f3f8e4d09d279a0d4e1a6876a2dca
Bodo171 <pbppbp17@gmail.com>
2021-03-01 16:48:35 +0200

    Add announcement

---
+++ srsi_back/static/announcements/4/content.jsx
@@ -0,0 +1,135 @@
+<ol style="font-weight:bold" type="I">
+    <li>
+        Listele finale cu participanții
+    </li>
+    <li> 
+        Scenariile de organizare
+    </li>
+    <li> 
+        Credențialele
+    </li>
+    <li> 
+        Sesiunea de antrenament
+    </li>
+</ol>
+
+<ol style="font-weight:bold" type="I">
+    <li>
+        Listele finale cu participanții
+        <p style="font-weight:normal">
+            Am publicat listele finale cu elevii, după reverificarea în județe. Le puteți găsi <a target="_blank" style="font-weight:bold" href={`${window.HOST}/static/participanti_final.pdf`}>aici</a>.
+        </p>
+    </li>
+
+    <li>
+        Scenariile de organizare
+        <p style="font-weight:normal">
+            Conform regulamentului și precizărilor ulterioare prin anunțurile din această secțiune, elevii pot susține proba în unul din următoarele moduri:
+        </p>
+        <ol style="font-weight:normal" type="a">
+            <li>
+                <p>
+                    Dacă școala îi informează că are posibilitatea de a-i primi în laboratoare pentru susținerea probei, elevii <b>pot alege</b> această variantă. 
+                    În acest caz, organizatorii din școală au de realizat următoarele:
+                    <ul>
+                        <li>
+                            Până la data de 11 martie pentru liceu și până la data de 18 martie pentru gimnaziu, vor trimite la adresa <a href="mailto:inscriere.scoli@sepi.ro">inscriere.scoli@sepi.ro</a> un tabel cu elevii care susțin proba în școală. 
+                            Lista trebuie să reflecte repartizarea pe săli a elevilor și trebuie să conțină, pentru fiecare elev, nume, prenume, școala, clasa. 
+                            De asemenea, trebuie indicat numele profesorului supraveghetor din sală.
+                        </li>
+                        <li>
+                            În fiecare sală de concurs se va realiza o filmare de ambient audio-video, care să cuprindă toți elevii din sală și profesorul/profesorii supraveghetori. 
+                            Nu este necesară captură de ecran pentru fiecare elev.
+                        </li>
+                        <li>
+                            Până la finalul zilei de concurs, filmarea de ambient trebuie încărcată într-un drive propriu în cloud și linkul trebuie trimis la adresa de email <a href="mailto:video.osepi@sepi.ro">video.osepi@sepi.ro</a>. 
+                            Mesajul trebuie să conțină, pentru fiecare elev care apare în filmare, <b>numele și clasa</b>.
+                        </li>
+                        <li>
+                            Trebuie să există un profesor pe hol care să însoțească elevii la baie. Nu este permis ca la baie să meargă mai mulți elevi deodată.
+                        </li>
+                    </ul>
+                </p>
+                <p>
+                    Decizia de organizare în școală a unui centru de concurs aparține strict fiecărei școli în parte, deci nu există obligativitate în acest sens. 
+                    Elevii trebuie să păstreze legătura cu profesorii pentru a se informa în legătură cu acest scenariu.
+                    <br />
+                    Și în acest caz, unii elevi pot alege să nu desfășoare proba în școală. 
+                    Elevii care nu apar pe listele trimise de școală se consideră că participă la probă în celălalt scenariu, descris în continuare.
+                </p>
+            </li>
+            <li>
+                <p>
+                    Elevii pot rămâne să desfășoare proba acasă. 
+                    În acest caz este nevoie să realizeze o filmare audio-video din lateral care să surprindă parte din fata elevului, mainile elevului, ecranul calculatorului și încăperea. 
+                    De asemenea trebuie realizată o captură de ecran cu activitatea desfășurată de elev în timpul probei. 
+                    Așadar este necesară o singură cameră de filmat, captura realizându-se intern cu softul instalat pe calculator. 
+                    Până la finalul zilei cu proba, produsele video trebuie încărcate într-un drive propriu în cloud iar linkurile trebuie trimise la adresa de email <a href="mailto:video.osepi@sepi.ro">video.osepi@sepi.ro</a>. 
+                    În mesaj trebuie evident indicat <b>numele elevului si clasa</b>.
+                </p>
+            </li>
+        </ol>
+    </li>
+
+    <li>
+        Credențialele
+        <p style="font-weight:normal">
+            În cursul zilei de miercuri, <b>3 martie</b> fiecare elev va primi <b>pe adresa de email pe care a transmis-o la înscriere</b>, datele de conectare la serverele de concurs. 
+            Atenție la păstrarea în siguranță a lor întrucât numele de utilizator și parola urmează să fie folosite la sesiunea de antrenament, la etapa județeană și la etapele ulterioare la care elevul se califică.
+            Începând cu <b>dimineața zilei de joi</b>, elevii trebuie să verifice primirea datelor iar în cazul în care acestea nu apar pot semnala problema la adresa conturi.osepi@sepi.ro. 
+            Dragi elevi, vă rugăm să ne ajutați și să vă ajutați în această etapă întrucât numărul mare de concurenți face ca răspunsul la mesajele prin care semnalați probleme să dureze mai mult. 
+            De exemplu, verificați exact adresa de email de pe care v-ați înscris (dacă aveți mai multe adrese), iar dacă ați trimis email cu o sesizare, așteptați răspuns până vineri dimineața, înainte de a reveni cu alt mesaj.
+        </p>
+    </li>
+
+    <li>
+        Sesiunea de antrenament
+        <p style="font-weight:normal">
+            În zilele de 6 și 7 martie (sâmbătă și duminică) concurenții vor avea acces la un server de concurs în aceleași condiții cu acelea de la probă. 
+            Pe server se află câte o problemă de antrenament pentru fiecare clasă. 
+            Rolul acestei acțiuni este să se testeze funcționarea conturilor dar și modul de trimitere a soluțiilor la o problemă. 
+            Pentru mai multe informații legate de interfața de concurent, puteți parcurge acest <a target="_blank" style="font-weight:bold" href={`${window.HOST}/static/tutorial.pdf`}>tutorial</a>.
+        </p>
+        <p style="font-weight:normal">
+            Sesiunea de antrenament începe în dimineața zilei de sâmbătă și <b>durează până duminică la ora 12</b>. 
+            <br/>
+            <b>Ora de început diferă de la o clasă la alta astfel:</b>
+        </p>
+        <ul>
+            <li>Clasa a X-a, sâmbătă la ora 9</li>
+            <li>Clasele a XI-a și a XII-a, sâmbătă la ora 9.30</li>
+            <li>Clasa a IX-a, sâmbătă la ora 10</li>
+            <li>Clasele a VI-a și a VII-a, sâmbătă la ora 10.30</li>
+            <li>Clasele a V-a și a VIII-a, sâmbătă la ora 11</li>
+        </ul>
+        <p>
+            Adresele pentru conectarea la servere vor fi anunțate pe siteul sepi.ro, la secțiunea 
+            anunțuri, sâmbătă dimineața.
+        </p>
+        <p style="font-weight:normal">
+            Dragi concurenții, nu trebuie să vă gândiți la problemele propuse ca având legătură cu subiectele de la probă, rolul lor este de a vă simula condițiile din concurs.
+            La finalul zilei de sâmbătă, pe sepi.ro la secțiunea anunțuri, vor fi postate descrieri ale soluțiilor.
+            Conectarea se poate face la orice oră din intervalul indicat, nu neapărat la prima oră. În medie 2-3 ore sunt suficiente pentru finalizarea sesiunii de antrenament. 
+            Acolo unde sunt probleme cu realizarea conectării, vă rugăm să le semnalați la una dintre adresele de email:
+        </p>
+        <ul style="font-weight:normal">
+            <li><a href="mailto:conturi.osepi5@gmail.com">conturi.osepi5@gmail.com</a></li>
+            <li><a href="mailto:conturi.osepi6@gmail.com">conturi.osepi6@gmail.com</a></li>
+            <li><a href="mailto:conturi.osepi7@gmail.com">conturi.osepi7@gmail.com</a></li>
+            <li><a href="mailto:conturi.osepi8@gmail.com">conturi.osepi8@gmail.com</a></li>
+            <li><a href="mailto:conturi.osepi9@gmail.com">conturi.osepi9@gmail.com</a></li>
+            <li><a href="mailto:conturi.osepi10@gmail.com">conturi.osepi10@gmail.com</a></li>
+            <li><a href="mailto:conturi.osepi1112@gmail.com">conturi.osepi1112@gmail.com</a></li>
+        </ul>
+        <p style="font-weight:normal">
+            Atenție, fiecare elev poate semnala probleme de conectare doar la adresa de mai sus asociată clasei în care se află. 
+            Aceste adrese de email se vor folosi doar în timpul sesiunii de antrenament.
+        </p>
+        <p style="font-weight:normal">
+            Pe concurenți îi rugăm să fie răbdători cu primirea răspunsului la mesaje, termenul limită fiind finalul zilei în care s-a trimis mesajul. 
+            De aceea, rugămintea este să se facă în primul rând verificarea primirii mesajului din data de 3 martie, acest lucru confirmând validitatea adresei de email trimisă la înscriere.
+            <br/>
+            Pentru sesiunea de antrenament concurenții pot accesa serverele de concurs de oriunde și nu se solicită de către noi filmare.
+        </p>
+    </li>
+</ol>
