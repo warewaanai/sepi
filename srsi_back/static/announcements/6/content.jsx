9aa1c34f82f3641883587e32939e1ee76ea7d123
Bogdan Pop <60036554+Bodo171@users.noreply.github.com>
2021-03-06 15:41:30 +0200

    Update content.jsx

--- srsi_back/static/announcements/6/content.jsx
+++ srsi_back/static/announcements/6/content.jsx
@@ -1,11 +1,11 @@
 <p>Zilele și orele de desfășurare a probelor la etapa județeană, secțiunea liceu.</p>
 
 <ul>
-<li><b>Clasa a X-a</b>: Vineri 12 martie ora 14.30. Concurenții se pot conecta începând cu ora 13.30. Subiectele vor fi disponibile de la 14.30. Proba durează 4 ore, deci până la ora 18.30.</li>
+<li><b>Clasa a X-a</b>: <b>Vineri</b> 12 martie ora 14.30. Concurenții se pot conecta începând cu ora 13.30. Subiectele vor fi disponibile de la 14.30. Proba durează 4 ore, deci până la ora 18.30.</li>
 
-<li><b>Clasele a XI-a și a XII-a</b>: Duminică 14 martie ora 8.30. Concurenții se pot conecta începând cu ora 7.30. Subiectele vor fi disponibile de la 8.30. Proba durează 4 ore, deci până la ora 12.30.</li>
+<li><b>Clasele a XI-a și a XII-a</b>: <b>Duminică</b> 14 martie ora 8.30. Concurenții se pot conecta începând cu ora 7.30. Subiectele vor fi disponibile de la 8.30. Proba durează 4 ore, deci până la ora 12.30.</li>
 
-<li><b>Clasa a IX-a</b>: Duminică 14 martie ora 14.30. Concurenții se pot conecta începând cu ora 13.30. Subiectele vor fi disponibile de la 14.30. Proba durează 4 ore, deci până la ora 18.30.</li>
+<li><b>Clasa a IX-a</b>: <b>Duminică</b> 14 martie ora 14.30. Concurenții se pot conecta începând cu ora 13.30. Subiectele vor fi disponibile de la 14.30. Proba durează 4 ore, deci până la ora 18.30.</li>
 </ul>
 
 <p>Urmăriți în continuare secțiunea anunțuri, vom reveni zilele următoare cu informații noi despre concurs.</p>
