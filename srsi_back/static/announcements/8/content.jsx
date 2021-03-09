4b20b36cbc78f45cabc3db0f2409456125b2816e
Bogdan Pop <60036554+Bodo171@users.noreply.github.com>
2021-03-09 22:05:01 +0200

    Add announcement #8

---
+++ srsi_back/static/announcements/8/content.jsx
@@ -0,0 +1,59 @@
+<p>
+    Pentru Online instituțional solicitările rămân neschimbate: o filmare pentru fiecare sală în care se organizează probă OSEPI. 
+    Filmarea trebuie să fie audio-video și să fie vizibili toți concurenții din sală dar și profesorul supraveghetor.
+</p>
+
+<p>
+    Pentru Online individual două variante:
+</p>
+
+<ol>
+    <li>
+        Două filmări, una fiind captura de ecran, cealaltă fiind filmarea camerei în care concurentul susține proba. 
+        Filmarea camerei trebuie să surprindă parte din fața concurentului, mâinile sale, dar și ecranul. 
+        Este evident că nu poate fi redat conținutul ecranului în cel mai mare detaliu, dar este important ca acesta să fie suficient de clar astfel încât să poată fi posibilă o sincronizare între filmarea externă și captura de ecran. 
+    </li>
+    <li>
+        O singură filmare care cuprinde captura camerei, atașate la calculatorul concurentului, suprapusă parțial pe captura ecranului.
+    </li>
+</ol>
+
+<p>Descriere detaliată:</p>
+
+<ul>
+    <li>
+        Pentru ambele variante filmarea care cuprinde captura de ecran se poate realiza cu un soft specializat. Un astfel de soft este OBS studio. 
+        Pentru a veni în ajutorul concurenților am realizat următorul <a target="_blank" style="font-weight:bold" href={`${window.HOST}/static/tutorial_obs.pdf`}> tutorial</a>.
+    </li>
+    <li>
+        Filmările care surprind activitățile concurentului trebuie să fie audio-video.
+    </li>
+    <li>
+        Se recomandă utilizarea de formate video care să comprime informația (de exemplu mp4, mpeg) rezultând fișiere video de dimensiuni decente.
+    </li>
+    <li>
+        Până la finalul zilei de concurs, produsele video trebuie încărcate într-un drive propriu în cloud (de exemplu Google Drive) și link-ul trebuie trimis printr-un email la adresa de email <b><a href="mailto:video.osepi@sepi.ro">video.osepi@sepi.ro</a></b>. 
+    </li>
+    <li>
+        În scenariul online individual, link-urile filmărilor concurenților  vor fi trimise de către fiecare concurent, de pe adresa de email cu care concurentul s-a înscris la concurs. 
+        La subiectul mesajului se va preciza obligatoriu: <b>numele, prenumele și clasa concurentului</b>.
+    </li>
+    <li>
+        În scenariul online instituțional, link-urile filmărilor  sălilor de concurs,  vor fi trimise de pe adresa de email a unui profesor care a înscris elevi în concurs.  
+        La subiectul mesajului se va preciza obligatoriu  școala, iar în conținut, pentru fiecare elev care apare în filmare, se va preciza <b>numele și clasa</b>.
+    </li>
+    <li>
+        Candidatul poate verifica funcționarea dispozitivului de filmare externă. 
+        În cazul în care filmarea se întrerupe, aceasta trebuie reluată. 
+        Este la latitudinea Comisiei Olimpiadei să stabilească dacă întreruperea filmării este suficient de mare încât concurentul să poată frauda.
+    </li>
+</ul>
+
+<p>
+    Dragi concurenți, procedura de filmare pe care o solicităm nu are rolul de a îngrădi în vreun fel participarea la Olimpiadă. 
+    Este doar rezultatul la care s-a ajuns luând în calcul, pe de o parte, dorința concurenților de a susține proba cât mai degajat posibil, iar pe de altă parte, solicitările multora de a se depune eforturi pentru a preveni frauda.
+</p>
+
+<p>
+Atenție - probele OSEPI nu se vor suprapune cu probele Olimpiadei Gazeta Matematică organizată de SSMR. :)
+</p>
