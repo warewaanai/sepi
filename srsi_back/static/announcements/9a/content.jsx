0ff358d218f580570f0e71266d02aff02864e79e
Bogdan Pop <60036554+Bodo171@users.noreply.github.com>
2021-03-11 22:13:47 +0200

    Repair announcement

--- srsi_back/static/announcements/9a/content.jsx
+++ srsi_back/static/announcements/9a/content.jsx
@@ -6,12 +6,12 @@
 <p><b>Atenție!</b> Enunțurile vor fi disponibile doar la linkul de mai sus, nu și în interfața de concurent.</p>
 <p>Este esențial să începeți filmarea până la ora 14.30, când începe concursul.</p>
 <p>Odată conectați, aveți acces de pe interfața de concurent la documentație C++.</p>
-<p>Beneficiați de secțiunea întrebări (Questions) pe interfață până la finalul concursului.</p>
+<p>Beneficiați de secțiunea întrebări (Communication/Comunicarea cu juriul) pe interfață până la finalul concursului.</p>
 <p>
     Ar trebui să nu fie probleme de conectare (acesta a fost unul dintre scopurile sesiunii de antrenament), 
     dar pentru situații excepționale puteți folosi totuși adresa de email <a href="mailto:conturi.osepi10@gmail.com">conturi.osepi10@gmail.com</a>.
 </p>
 <p>Reamintim că în timpul probei de concurs sunt permise până la 30 de submisii la o problemă. Intervalul de timp dintre două submisii trebuie să fie de cel puțin un minut.</p>
 <p>Punctajul final la o problemă este dat de cel mai mare punctaj obținut dintre toate submisiile la acea problemă.</p>
-<p>Vă recomandăm să vă salvați link-ul de Google Drive în cazul unor posibile situații neprevăzute cu site-ul SEPI</p>
+<p>Vă recomandăm să vă salvați link-ul de Google Drive în cazul unor posibile situații neprevăzute cu site-ul SEPI.</p>
 
