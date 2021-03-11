39b06c982f391f2d77dba4993045cc685e6ea0a2
Bodo171 <pbppbp17@gmail.com>
2021-03-11 17:10:51 +0200

    Add announcement #9

---
+++ srsi_back/static/announcements/9/content.jsx
@@ -0,0 +1,12 @@
+<p>Zilele și orele de desfășurare a probelor la etapa județeană, secțiunea gimnaziu.</p>
+
+<p>
+    <b>Clasele a V-a și a VI-a: Duminică </b> 21 martie ora 9.30. Concurenții se pot conecta începând cu ora 8.30. 
+    Subiectele vor fi disponibile de la 9.30. Proba durează 3 ore, deci până la ora 12.30.
+</p>
+
+<p>
+<b>Clasele a VII-a și a VIII-a: Duminică</b> 21 martie ora 14.30. 
+Concurenții se pot conecta începând cu ora 13.30. Subiectele vor fi disponibile de la 14.30. 
+Proba durează 3 ore, deci până la ora 17.30.
+</p>
