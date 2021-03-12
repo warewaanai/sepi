8824e12bc7df2bf85d5f9875f758a286f4e97b02
Bodo171 <pbppbp17@gmail.com>
2021-03-12 19:06:00 +0200

    Add results

--- srsi_back/result_fetcher.py
+++ srsi_back/result_fetcher.py
@@ -7,7 +7,7 @@ def process_line(raw_line: str):
     contestant_name = line[1]
     raw_scores = line[2:]
     scores = [float(raw_scores[i]) for i in range(0, len(raw_scores), 2)] 
-    a = [county, level, contestant_name]
+    a = [county, level, contestant_id]
     a.extend(scores)
     return a
 
@@ -15,7 +15,45 @@ def process_head(raw_line: str):
     line = raw_line.split(',')
     raw_scores = line[2:]
     scores = [raw_scores[i] for i in range(0, len(raw_scores), 2)] 
-    a = ["Judet", "Clasa", "Nume"]
+    a = ["Judet", "Clasa", "ID"]
+    a.extend(scores)
+    return a
+
+in_file = open("ranking.csv", "r")
+out_filename = input("File name:")
+
+lines = in_file.readlines()
+
+table = {"head": process_head(lines[0])}
+content = []
+for line in lines[1:]:
+    content.append(process_line(line))
+content.sort(key= lambda entry: -entry[-1])
+table["body"] = content
+
+print(table)
+with open(out_filename, "w") as out_file:
+    json.dump(table, out_file)
+
+'''
+import json
+
+def process_line(raw_line: str):
+    line = raw_line.split(',')
+    contestant_id = line[0]
+    county, level, _ = contestant_id.split('_')
+    contestant_name = line[1]
+    raw_scores = line[2:]
+    scores = [float(raw_scores[i]) for i in range(0, len(raw_scores), 2)] 
+    a = [county, level, contestant_id]
+    a.extend(scores)
+    return a
+
+def process_head(raw_line: str):
+    line = raw_line.split(',')
+    raw_scores = line[2:]
+    scores = [raw_scores[i] for i in range(0, len(raw_scores), 2)] 
+    a = ["Judet", "Clasa", "ID"]
     a.extend(scores)
     return a
 
@@ -31,3 +69,4 @@ for line in lines[1:]:
 print(table)
 with open(out_filename, "w") as out_file:
     json.dump(table, out_file)
+'''
