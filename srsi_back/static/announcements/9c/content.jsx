68d2d5d81f3ce7e84e7737ae16859bce7b73425b
Bodo171 <pbppbp17@gmail.com>
2021-03-13 16:06:09 +0200

    Add announcements

---
+++ srsi_back/static/announcements/9c/content.jsx
@@ -0,0 +1,24 @@
+<p>Dragi concurenți,</p>
+<p>
+    <b>
+        Pentru clasa 9-a, vă rugăm să trimiteți filmările la adresa
+        <a href="mailto:conturi.osepi9@gmail.com">conturi.osepi9@gmail.com</a> în locul adresei precizate în anunțurile precendente.
+    </b>
+    </p>
+<p></p>
+<p>
+    Pentru proba de la etapa județeană OSEPI clasa a 9-a trebuie să intrați pe linkul <b><a href="https://drive.google.com/drive/folders/15DE7wDu0v8NiwaX1CVPoGCcFYX6OZCDW?usp=sharing">acesta</a></b> 
+    unde veți găsi toate cele necesare (citiți mai întâi documentul Instrucțiuni).
+</p>
+<p><b>Atenție!</b> Enunțurile vor fi disponibile doar la linkul de mai sus, nu și în interfața de concurent.</p>
+<p>Este esențial să începeți filmarea până la ora 14.30, când începe concursul.</p>
+<p>Odată conectați, aveți acces de pe interfața de concurent la documentație C++.</p>
+<p>Beneficiați de secțiunea întrebări (Communication/Comunicarea cu juriul) pe interfață până la finalul concursului.</p>
+<p>
+    Ar trebui să nu fie probleme de conectare (acesta a fost unul dintre scopurile sesiunii de antrenament), 
+    dar pentru situații excepționale puteți folosi totuși adresa de email <a href="mailto:conturi.osepi9@gmail.com">conturi.osepi9@gmail.com</a>.
+</p>
+<p>Reamintim că în timpul probei de concurs sunt permise până la 30 de submisii la o problemă. Intervalul de timp dintre două submisii trebuie să fie de cel puțin un minut.</p>
+<p>Punctajul final la o problemă este dat de cel mai mare punctaj obținut dintre toate submisiile la acea problemă.</p>
+<p>Vă recomandăm să vă salvați link-ul de Google Drive în cazul unor posibile situații neprevăzute cu site-ul SEPI.</p>
+
