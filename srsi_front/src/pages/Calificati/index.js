9b193ac69edff28d1345659533fb8fe2dfea6d25
Bodo171 <pbppbp17@gmail.com>
2021-03-20 22:16:06 +0200

    Fix caching

--- srsi_front/src/pages/Calificati/index.js
+++ srsi_front/src/pages/Calificati/index.js
@@ -6,16 +6,16 @@ const CalificatiPage = () =>{
         <h1><TermBlink blink>Calificați la etapa națională</TermBlink></h1>
         <ul>
            <li>
-                <a target="_blank" href={`${window.HOST}/static/qualified/9.pdf`}>Clasa a 9-a </a>
+                <a target="_blank" href={`${window.HOST}/static/qualified/9q.pdf`}>Clasa a 9-a </a>
            </li>
            <li>
-                <a target="_blank" href={`${window.HOST}/static/qualified/10.pdf`}>Clasa a 10-a</a>
+                <a target="_blank" href={`${window.HOST}/static/qualified/10q.pdf`}>Clasa a 10-a</a>
            </li>
            <li>
-                <a target="_blank" href={`${window.HOST}/static/qualified/11.pdf`}>Clasa a 11-a</a>
+                <a target="_blank" href={`${window.HOST}/static/qualified/11q.pdf`}>Clasa a 11-a</a>
            </li>
            <li>
-                <a target="_blank" href={`${window.HOST}/static/qualified/12.pdf`}>Clasa a 12-a</a>
+                <a target="_blank" href={`${window.HOST}/static/qualified/12q.pdf`}>Clasa a 12-a</a>
            </li>
         </ul>
     </>
