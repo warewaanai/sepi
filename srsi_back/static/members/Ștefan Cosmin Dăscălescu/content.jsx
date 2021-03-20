9dd51e316284f2d872cb165e15f221b6cb628208
Bodo171 <pbppbp17@gmail.com>
2021-03-20 13:18:26 +0200

    Add member

---
+++ srsi_back/static/members/Ștefan Cosmin Dăscălescu/content.jsx
@@ -0,0 +1,31 @@
+<p>
+    Mă numesc Dăscălescu Ștefan-Cosmin și sunt student în anul I la Facultatea de
+    Matematică și Informatică, Universitatea din Bucuresti.
+</p>
+<p>
+    M-am calificat la Olimpiada Naționala de Informatică în fiecare an începand din 2013
+    pâna în 2020, obținând printre altele, 4 mențiuni(2016, 2017, 2018, 2019), medalie de
+    aur(2018), medalie de argint(2016, 2017, 2019) dar si medalie de bronz(2015).
+</p>
+<p>
+    Am obtinut medalia de bronz la Olimpiada Internationala de Informatică pe
+    echipe(IIOT) in 2019.
+</p>
+<p>
+    Pe lânga aceste olimpiade, am mai obtinut distincții la multiple concursuri
+    internaționale si naționale, precum Adolescent Grigore Moisil (AGM), Urmașii lui Moisil,
+    Prosoft@NT sau Info(1) Cup.
+</p>
+<p>
+    Co-autor la doua runde de Codeforces(#554 si #676).
+</p>
+<p>
+    Începând din anul scolar 2020-2021 sunt membru al comisiei științifice a Olimpiadei
+    Naționale de Informatica pe echipe (IIOT).
+</p>
+<p>
+    Nu in ultimul rand, am un canal de youtube(Stefan Dascalescu) unde prezint soluții la
+    diverse probleme de algoritmică, iar in timpul meu liber pregatesc elevi de liceu la
+    informatică în cadrul Centrului Judetean de Excelenta Neamț.
+</p>
+
