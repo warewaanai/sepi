d6458d2bc1bffa98097d21c84abab0ab32e2ff25
Bodo171 <pbppbp17@gmail.com>
2021-03-21 21:03:32 +0200

    Change qualified page

--- srsi_front/src/pages/Rezultate/listLevels.js
+++ srsi_front/src/pages/Rezultate/listLevels.js
@@ -1,8 +1,8 @@
-const listLevels = () => {
+const listLevels = (reduced=false) => {
     let levels = [];
     for(let i = 5; i<=12; i+=1)
         levels.push(i.toString());
-    levels.push("baraj");
+    if(!reduced) levels.push("baraj");
     return levels;
 }
 
