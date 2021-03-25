14ad563268937dab7a888331cba69528655577ae
Bogdan Pop <60036554+Bodo171@users.noreply.github.com>
2021-03-25 13:04:00 +0200

    Add sponsors (#4)
    * Add sponsors
    
    * Add different container styles

---
+++ srsi_front/src/components/sponsorcard/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './stylesheet.scss';
+
+const SponsorCard = props => {
+    console.log(props);
+    let containerType = "SponsorLogoContainer";
+    if(props.horizontal)
+        containerType = "SponsorHorizontalLogoContainer";
+    return <a class="SponsorLink" href={props.link}>
+        <div class="SponsorCard">
+                <div class={containerType}>
+                    <img class="SponsorLogo" src={props.logo}/>
+                </div>
+            <div class="SponsorName">
+                {props.name}
+            </div>
+        </div>
+    </a>
+}
+
+export default SponsorCard;
---
+++ srsi_front/src/pages/Sponsori/index.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect }  from 'react';
+import SponsorCard from '../../components/sponsorcard';
+import TermBlink from '../../components/termblink';
+
+const SponsorsPage = props =>{
+    const [sponsors, setSponsors] = useState()
+    useEffect(() => {fetch(window.HOST + "/api/list_sponsors").then(res => res.json()).then(res => setSponsors(res))}, [])
+    let sponsorcards = [];
+    for (const key in sponsors) {
+        const sponsor = JSON.parse(sponsors[key]);
+        console.log(sponsor);
+        sponsorcards.push(
+            <SponsorCard
+                name={key}
+                logo={window.HOST + sponsor.logo}
+                link={sponsor.link}
+                horizontal={sponsor.horizontal}
+            />
+        );
+
+    }
+    return <>
+        <h1> <TermBlink blink>Sponsorii SEPI</TermBlink> </h1>
+        <div id="SponsorTable">
+            {sponsorcards}
+        </div>
+    </>
+}
+
+export default SponsorsPage;
