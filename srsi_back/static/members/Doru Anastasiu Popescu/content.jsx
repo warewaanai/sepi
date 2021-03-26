1b588bee74ad5b99f7f7fc1e869806fc784052e9
Bodo171 <pbppbp17@gmail.com>
2021-03-26 21:08:52 +0200

    Add member

---
+++ srsi_back/static/members/Doru Anastasiu Popescu/content.jsx
@@ -0,0 +1,48 @@
+
+<ul>
+    <li>
+        Conf. univ. dr. - Universitatea din Piteşti, Departamentul de Matematică-Informatică, Pitești 
+    </li>
+    <li>
+        Director  CENTRUL DE CERCETARE ŞTIINȚIFICĂ ”Modele de calcul si metodologii de programare” (MCMP)  Universitatea din Piteşti - Departamentul de Matematică-Informatică
+    </li>
+    <li>
+        Doctor în informatică - titlu teză: "APLICAŢII WEB. Modelare, Specificaţii, Complexitate, Testare şi Verificare, 2012" 
+    </li>
+    <li>
+        Odinul Meritul pentru Învăţământ în gradul de CAVALER, 2004
+    </li>
+    <li>
+        Coordonator/președinte Info-Oltenia 2002- 2016
+    </li>
+    <li>
+        Organizator tabără lot olimpic de informatică, Slatina, 2003
+    </li>
+    <li>
+        Distincția ”Gheorghe Lazăr” clasa a III-a, 2007
+    </li>
+    <li>
+        Organizator/vicepreședinte Olimpiadă Naţională de Informatică, 2010, Slatina
+    </li>
+    <li>
+        Membru în comisia științifică a Balcaniadei de Informatică, 2013
+    </li>
+    <li>
+        Profesorul anului în Județul OLT,  2018
+    </li>
+    <li>
+        Membru Comisia Naţională de Informatică
+    </li>
+    <li>
+        Coordonator al echipei României la concursuri internaţionale de informatică: CEOI 2001- Ungaria,  CEOI 2002- Slovacia, CEOI 2003- Germania, CEOI 2004- Polonia, CEOI 2005- Ungaria, BOI 2006- Cipru, BOI 2007- Moldova, CEOI 2011- Polonia, BOI 2012- Serbia, Turneul Shumen 2014 – Bulgaria
+    </li>
+    <li>
+        Coordonator echipe de robotica participante la Olimpiada Mondiala de Robotica din India – 2016, Thailanda – 2018, Open-ul First Lego League Uruguay - 2019
+    </li>
+    <li>
+        Peste 25 manuale și culegeri de informatica si TIC pentru liceu și gimnaziu
+    </li>
+    <li>
+        Peste 80 articole științifice din domeniile: combinatorică, teoria grafurilor, algoritmi, limbaje de programare (C++, Java), verificare și testare, programare web, aplicații web, e-learning, programarea roboților
+    </li>
+</ul>
