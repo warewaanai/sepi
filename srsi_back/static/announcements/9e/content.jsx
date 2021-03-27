94093bfdd73509ac3812e820c2df374447f7532b
Bodo171 <pbppbp17@gmail.com>
2021-03-27 11:15:06 +0200

    Fix article and add announcement

---
+++ srsi_back/static/announcements/9e/content.jsx
@@ -0,0 +1,62 @@
+<p>
+    Dragi concurenți,
+</p>
+    
+<p>
+Programul etapei naționale OSEPI este următorul:
+</p>
+
+<p>
+    <b>Clasele a XI-a și a XII-a: Miercuri</b> 7 aprilie ora 8.30. Concurenții se pot conecta începând cu ora 7.30. Subiectele vor fi disponibile de la 8.30. Proba durează 4 ore, deci până la ora 12.30.
+</p>
+
+<p>
+    <b>Clasele a IX-a și a X-a: Miercuri</b> 7 aprilie ora 14.30. Concurenții se pot conecta începând cu ora 13.30. Subiectele vor fi disponibile de la 14.30. Proba durează 4 ore, deci până la ora 18.30.
+</p>
+
+<p>
+    <b>Clasele a V-a și a VI-a: Joi</b> 8 aprilie ora 8.30. Concurenții se pot conecta începând cu ora 7.30. Subiectele vor fi disponibile de la 8.30. Proba durează 4 ore, deci până la ora 12.30.
+</p>
+
+<p>
+    <b>Clasele a VII-a și a VIII-a: Joi</b> 8 aprilie ora 14.30. Concurenții se pot conecta începând cu ora 13.30. Subiectele vor fi disponibile de la 14.30. Proba durează 4 ore, deci până la ora 18.30.
+</p>
+
+<p>
+    <b>Baraj 1 Liceu: Vineri</b> 9 aprilie ora 9.30. Concurenții se pot conecta începând cu ora 8.30. Subiectele vor fi disponibile de la 9.30. Proba durează 5 ore, deci până la ora 14.30.
+</p>
+
+<p>
+    <b>Baraj Gimnaziu: Duminică</b> 11 aprilie ora 9.30. Concurenții se pot conecta începând cu ora 8.30. Subiectele vor fi disponibile de la 9.30. Proba durează 4 ore, deci până la ora 13.30.
+</p>
+
+<p>
+    <b>Baraj 2 Liceu: Duminică</b> 11 aprilie ora 9.30. Concurenții se pot conecta începând cu ora 8.30. Subiectele vor fi disponibile de la 9.30. Proba durează 5 ore, deci până la ora 14.30.
+</p>
+
+<p>
+    La probele de baraj pentru formarea loturilor naționale participă toți concurenții care au obținut punctaj nenul în cadrul probei de la etapa națională.
+    Conform regulamentului, în realizarea clasamentelor finale, nu se iau în calcul coeficienți proveniți din punctajele probelor pe clase.
+</p>
+
+<div color="blue">
+<p>
+    Ținând cont de evoluția pandemiei nu se pune problema obligativității organizării etapei naționale în de centre de concurs, rămân așadar valabile aceleași scenarii ca la etapa județeană.
+</p>
+
+<p>
+    Vor fi câteva modificări privind monitorizarea video:
+</p>
+<ul>
+    <li>
+        Este obligatoriu scenariul care implică filmare separată din lateral, din care să se poată vedea: mâinile, parte din figură, parte din ecran (pentru detalii vă rugăm să citiți anunțul despre <b>monitorizarea video</b>, din data de 9 martie, rămânând valabilă doar varianta 1 dintre cele două descrise acolo);
+    </li>
+    <li>
+        În cazul folosirii scenariului Online Instituțional este obligatorie, pe lângă filmarea de ambient, câte o “filmare” - captură de ecran pentru fiecare concurent din sală.
+    </li>
+</ul>
+
+<p>
+Vom reveni în curând cu un nou anunț detaliat despre monitorizarea video, care va conține și un tutorial / filmuleț demonstrativ despre felul în care trebuie să arate filmarea.
+</p>
+</div>
