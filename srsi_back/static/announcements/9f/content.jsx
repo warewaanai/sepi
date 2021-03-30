e6c10f9ae4e56a28f1efc8fd980d4f74b536862c
Bodo171 <pbppbp17@gmail.com>
2021-03-30 20:44:00 +0300

    Add announcement 9f

---
+++ srsi_back/static/announcements/9f/content.jsx
@@ -0,0 +1,42 @@
+<p>Dragi concurenți,</p>
+
+<p>
+În data de <b>2 aprile 2021</b> se va organiza o Sesiune de antrenament dedicată elevilor care vor participa la Etapa de selecție a loturilor de <b>seniori</b> (organizată în 9 și 11 aprile ).
+</p>
+
+<p>
+Scopul Sesiunii de antrenament este familiarizarea concurenților cu tipurile de task-uri utilizate la competițiile internaționale, tipuri de taskuri au fost utilizate și în selecții organizate în anii trecuți, dar dorim să exemplificăm funcționarea acestora pe CMS.
+</p>
+
+<p style="font-weight:bold">
+Atenție - se va organiza sesiune de antrenament doar pentru selecția lorurilor de seniori. Task-urile utilizate la secțiunea juniori vor avea formatul cunoscut din etapele anterioare.
+</p>
+
+<p>
+Pentru a vă acomoda cu tipurile de task-uri va recomandăm să studiați <a href=" https://cms.readthedocs.io/en/v1.4/Task%20types.html">documentația CMS</a>
+și <a href="https://infopro.lrmd.ro/anunt.pdf" > tutorialul</a> publicat la concursul InfoPro grupa B.
+</p>
+
+<p>
+Pentru sesiunea de antrenament care începe vineri 2 aprilie  trebuie să intrați pe linkul <a href="https://drive.google.com/drive/u/1/folders/1tnaxS9q-1mQiR-LwBTUFqw5PWHDdFy6O">acesta</a>  unde veți găsi enunțurile și linkurile către platforma de concurs (citiți documentul <i>Instrucțiuni</i>) la ora 11.30.
+</p>
+
+<p>
+Atenție! Enunțurile vor fi disponibile doar la linkul de mai sus, nu și în interfața de concurent.
+</p>
+
+<p>
+Vă puteți conecta și lucra vineri 2 aprilie în orice interval de la ora 12.00 până la ora 24.00 .
+</p>
+
+<p>
+Odată conectați, aveți acces de pe interfața de concurent la documentație C++.
+</p>
+
+<p>
+Din cauza numărului foarte mare de concurenți, există posibilitatea de blocare a serverelor, în special în primele ore, așadar vă rugăm să fiți răbdători și să reveniți după un timp.
+</p>
+
+
+
+
