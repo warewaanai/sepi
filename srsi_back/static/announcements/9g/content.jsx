6e298d748f07b0247e1c67a65f8f524307e3a1e8
Bodo171 <pbppbp17@gmail.com>
2021-03-31 13:20:39 +0300

    Add announcement 9g

---
+++ srsi_back/static/announcements/9g/content.jsx
@@ -0,0 +1,50 @@
+
+<p>
+    Dragi concurenți,
+</p>
+
+<p>
+    Pentru etapa națională OSEPI (care își desfășoară probele săptămâna viitoare) rămân valabile cele două scenarii de organizare de la etapa județeană: online individual și online instituțional. Dacă doriți susținerea probei în scenariul online instituțional, trebuie să vă adresați școlilor voastre, însă acestea nu au obligativitatea organizării probei, mai cu seamă că există un ordin de ministru care a stabilit vacanță datorată situației sanitare.
+</p>
+
+<p>
+    Pentru etapa națională, la monitorizarea video, apar unele <b>modificări</b> comparativ cu etapa județeană:
+</p>
+
+<ul>
+    <li>
+        La scenariul online individual este acceptată doar varianta 1 propusă în anunțul cu monitorizarea video din data de 9 martie. Reluăm din acel anunț: <br /> <br />
+        Două filmări, una fiind captura de ecran, cealaltă fiind filmarea camerei în care concurentul susține proba. 
+        Filmarea camerei, din lateral, trebuie să surprindă parte din fața concurentului, mâinile sale, dar și ecranul. 
+        Este evident că nu poate fi redat conținutul ecranului în cel mai mare detaliu, dar este important ca acesta să fie suficient de clar astfel încât să poată fi posibilă o sincronizare între filmarea externă și captura de ecran. 
+        Pentru exemplificare, urmăriți filmele model de la acest <a target="_blank" style="font-weight:bold" href='https://drive.google.com/drive/folders/1x6pKculuE4wQd29E1JJGetQeP6CQfIBV?usp=sharing'>link</a>  (filmarea laterală și captura). 
+        În modelul prezentat figura concurentului este blurată însă în filmele trimise de voi aceasta trebuie să fie vizibilă. 
+        <b>La începutul probei trebuie să vă îndreptați către cameră și să vă prezentați.</b>
+    </li>
+</ul>
+
+
+<p>
+Pentru realizarea capturii poate fi folosit OBS Studio care poate cuprinde și figura concurentului din față (opțional figura din față), 
+însă filmarea laterală ca în model este obligatorie. Puteți folosi tutorialul pentru configurarea OBS, disponibil <a target="_blank" style="font-weight:bold" href={`${window.HOST}/static/tutorial_obs.pdf`}>aici</a>.
+</p>
+
+<ul>
+    <li>
+        La scenariul online instituțional, pe lângă filmarea sălii în ansamblu, este necesară realizarea unei capturi de ecran pentru fiecare concurent în parte. 
+        Filmarea în ansamblu trebuie să fie audio-video și aceasta să surprindă toți concurenții din sală dar și profesorul supraveghetor.
+    </li>
+</ul>
+
+<p>
+    Așadar, filmările trebuie să fie audio-video. Nu este permisă folosirea căștilor.
+</p>
+
+<p>
+    Nerespectarea regulilor referitoare la monitorizarea video poate conduce la descalificarea concurentului. 
+    Validarea elevilor calificați în lotul național în urma probelor de baraj este condiționată și de validarea lor în urma probelor pe clase de la etapa națională.
+</p>
+
+<p>
+    Vom reveni mai aproape de momentul probelor cu un anunț despre modul de trimitere a celor două filmări (va fi unul asemănător cu modalitatea utilizată la gimnaziu, adică se va folosi un formular în care concurenții completează date de identificare și link la filmări).
+</p>
