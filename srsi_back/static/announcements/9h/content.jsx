eb7dfa6f26ef5f3cb275cce09b9283f58123b2bb
Bodo171 <pbppbp17@gmail.com>
2021-04-04 16:22:50 +0300

    Add announcement 9h

---
+++ srsi_back/static/announcements/9h/content.jsx
@@ -0,0 +1,311 @@
+<p>Dragi concurenți,</p>
+
+<p>
+    Începând cu ziua de miercuri au loc rundele etapei naționale a OSEPI: probele pe clase apoi barajele de selecție a loturilor naționale de seniori respectiv juniori.
+</p>
+
+<p>
+    Vă rugăm să citiți cu atenție acest anunț, atât secțiunea dedicată clasei la care participați dar și precizările generale.
+</p>
+
+<div style="font-weight:bold">
+<p>
+    Pentru trimiterea filmărilor vom folosi un formular în care veți indica linkul (linkurile) la care acestea pot fi vizualizate (pentru gimnaziu s-a folosit această metodă și la etapa județeană, însă sunt unele modificări). 
+    Vă rugăm așadar să nu puneți în formular arhive pe care să le descărcăm ci linkuri la care să se poată vizualiza direct.
+    <span style="color:blue">
+        Formularul are două secțiuni, prima adresată elevilor care au susținut proba de acasă - online individual, iar a doua corespunzătoare celor care au susținut proba în școală sub supravegherea profesorului - online instituțional. 
+        Doar una dintre ele se va completa (pentru câmpurile din cealaltă secțiune completați cu -) și se va trimite.
+    </span>
+    Atenție că sunt modificări față de regulile de filmare de la etapa județeană, prezentate pe larg în anunțul anterior, din data de 31 martie. 
+    Le reluăm pe cele mai importante:
+</p>
+
+<ul>
+    <li>
+        La online individual trebuie două filmări, una captură de ecran și una laterală, ca în filmulețul demonstrativ din anunțul anterior. Acestea NU trebuie suprapuse (astfel ecranul din filmarea laterală ar deveni foarte mic și nu ar mai fi vizibil).
+    </li>
+
+    <li>
+    La online instituțional sunt necesare capturi de ecran pentru toți elevii din sală.
+    </li>
+</ul>
+
+<p>
+    Vă rugăm să lucrați relaxat dar totodată să aveți în timpul probei o atitudine care să nu trezească suspiciuni. 
+    Filmarea trebuie să fie audio-video. Nu este permisă folosirea căștilor.
+</p>
+</div>
+
+<p>
+    Înainte de prezentarea detaliilor concrete pentru fiecare clasă în parte, vă aducem la cunoștință câteva lucruri general valabile:
+</p>
+
+<ul>
+<li>
+    Odată conectați, aveți acces de pe interfața de concurent la documentație C++.
+</li>
+<li>
+    Beneficiați de secțiunea întrebări (Communication/Comunicarea cu juriul) pe interfață până la finalul concursului.
+</li>
+<li>
+    Urmăriți secțiunea “Communication/Comunicarea cu juriul” și pentru a vedea eventualele anunțuri generale din timpul probei.
+</li>
+<li>
+    La secțiunea “Statement” din interfața de concurent găsiți restricțiile pentru fiecare problemă (Timpul de rulare pe test și Memoria).
+</li>
+<li>
+    Reamintim că în timpul probei de concurs sunt permise până la 30 de submisii la o problemă. 
+    Intervalul de timp dintre două submisii trebuie să fie de cel puțin un minut.
+</li>
+</ul>
+<br />
+<div>
+<b>
+    Pentru clasa 5-a
+</b>
+<p>
+    Proba se va desfășura joi 8 aprilie începând cu ora 8.30, va dura 4 ore și va consta din trei probleme de natură algoritmică.
+</p>
+<p>
+    Modul de desfășurare este asemănător cu acela de la etapa județeană.
+</p>
+
+<p>
+    Vă rugăm să trimiteți filmările completând următorul <a target="_blank" style="font-weight:bold" href="https://forms.gle/1dhgnWjWFoLHaSYD6">formular</a>. 
+    Trebuie să completați formularul pentru fiecare filmare.
+</p>
+<p>
+    Pentru proba de la etapa națională OSEPI clasa a 5-a trebuie să intrați pe linkul <a target="_blank" style="font-weight:bold" href="https://drive.google.com/drive/folders/1plgJYLtSU3kK0FhNjbXzos1UcH09_1YM?usp=sharing">acesta</a>
+    unde veți găsi toate cele necesare (citiți mai întâi documentul Instrucțiuni).
+</p>
+<p>
+    <b>Atenție!</b> Enunțurile vor fi disponibile doar prin intermediul linkului de mai sus, nu și la secțiunea <i>Statement</i> a interfeței de concurent.
+</p>
+<p>
+    Este esențial să porniți filmarea înainte de ora 8.30, când începe concursul.
+</p>
+<p>
+    Ar trebui să nu fie probleme de conectare (a existat anterior sesiunea de antrenament și etapa județeană), dar pentru situații excepționale puteți folosi totuși adresa de email 
+    <a href="mailto:conturi.osepi5@gmail.com">conturi.osepi5@gmail.com</a>.
+</p>
+<p>
+    Ca și la etapa județeană, punctajul la o problemă este cel mai mare dintre toate submisiile de la acea problemă.
+</p>
+</div>
+<br />
+<div>
+<b>Pentru clasa 6-a</b>
+<p>
+    Proba se va desfășura joi 8 aprilie începând cu ora 8.30, va dura 4 ore și va consta din trei probleme de natură algoritmică.
+</p>
+<p>
+    Modul de desfășurare este asemănător cu acela de la etapa județeană.
+</p>
+<p>
+    Vă rugăm să trimiteți filmările completând următorul <a target="_blank" style="font-weight:bold" href="https://forms.gle/mBUTpEFzRr29U9vK6">formular</a> 
+    Trebuie să completați formularul pentru fiecare filmare.
+</p>
+<p>
+    Pentru proba de la etapa națională OSEPI clasa a 6-a trebuie să intrați pe linkul <a target="_blank" style="font-weight:bold" href="https://drive.google.com/drive/folders/168AKT0HQUzVGZideXwQglDh3XJmzeJJw?usp=sharing">acesta</a> 
+    unde veți găsi toate cele necesare (citiți mai întâi documentul Instrucțiuni).
+</p>
+<p>
+    <b>Atenție!</b> Enunțurile vor fi disponibile doar prin intermediul linkului de mai sus, nu și la secțiunea <i>Statement</i> a interfeței de concurent.
+</p>
+<p>
+    Este esențial să porniți filmarea înainte de ora 8.30, când începe concursul.
+</p>
+<p>
+    Ar trebui să nu fie probleme de conectare (a existat anterior sesiunea de antrenament și etapa județeană), 
+    dar pentru situații excepționale puteți folosi totuși adresa de email <a href="mailto:conturi.osepi6@gmail.com">conturi.osepi6@gmail.com</a>.
+</p>
+<p>
+    Ca și la etapa județeană, punctajul la o problemă este cel mai mare dintre toate submisiile de la acea problemă.
+</p>
+</div>
+<br />
+<div>
+    <b>Pentru clasa 7-a</b>
+    <p>
+        Proba se va desfășura joi 8 aprilie începând cu ora 14.30, va dura 4 ore și va consta din trei probleme de natură algoritmică.
+    </p>
+    <p>
+        Modul de desfășurare este asemănător cu acela de la etapa județeană.
+    </p>
+    <p>
+        Vă rugăm să trimiteți filmările completând următorul <a target="_blank" style="font-weight:bold" href="https://docs.google.com/forms/d/e/1FAIpQLSccOI8TD1zwCfOK7Q-E4qlwEaqzzM787cAKzRmBmrhMIghXPw/viewform?usp=sf_link">formular</a>. 
+        Trebuie să completați formularul pentru fiecare filmare.
+    </p>
+    <p>
+        Pentru proba de la etapa națională OSEPI clasa a 7-a trebuie să intrați pe linkul <a target="_blank" style="font-weight:bold" href="https://drive.google.com/drive/folders/1ikAqIP2QgS_Uvxr2KrWv00-GVmsLJI1m?usp=sharing">acesta</a>
+        unde veți găsi toate cele necesare (citiți mai întâi documentul Instrucțiuni).
+    </p>
+    <p>
+        <b>Atenție!</b> Enunțurile vor fi disponibile doar prin intermediul linkului de mai sus, nu și la secțiunea <i>Statement</i> a interfeței de concurent.
+    </p>
+    <p>
+        Este esențial să porniți filmarea înainte de ora 14.30, când începe concursul.
+    </p>
+    <p>
+        Ar trebui să nu fie probleme de conectare (a existat anterior sesiunea de antrenament și etapa județeană), 
+        dar pentru situații excepționale puteți folosi totuși adresa de email <a href="mailto:conturi.osepi7@gmail.com">conturi.osepi7@gmail.com</a>.
+    </p>
+    <p>
+        Ca și la etapa județeană, punctajul la o problemă este cel mai mare dintre toate submisiile de la acea problemă.
+    </p>
+</div>
+<br />
+<div>
+    <b>Pentru clasa 8-a</b>
+    <p>
+        Proba se va desfășura joi 8 aprilie începând cu ora 14.30, va dura 4 ore și va consta din trei probleme de natură algoritmică.
+    </p>
+    <p>
+        Modul de desfășurare este asemănător cu acela de la etapa județeană.
+    </p>
+    <p>
+        Vă rugăm să trimiteți filmările completând următorul <a target="_blank" style="font-weight:bold" href="https://forms.gle/D3Ahc53LUQ4aNUfo7">formular</a>. 
+        Trebuie să completați formularul pentru fiecare filmare.
+    </p>
+    <p>
+        Pentru proba de la etapa națională OSEPI clasa a 8-a trebuie să intrați pe linkul <a target="_blank" style="font-weight:bold" href="https://drive.google.com/drive/folders/152Mekqtp024uYPwF7oE90YgM9iO9dnsQ?usp=sharing">acesta </a>
+        unde veți găsi toate cele necesare (citiți mai întâi documentul Instrucțiuni).
+    </p>
+    <p>
+        <b>Atenție!</b> Enunțurile vor fi disponibile doar prin intermediul linkului de mai sus, nu și la secțiunea Statement a interfeței de concurent.
+    </p>
+    <p>
+        Este esențial să porniți filmarea înainte de ora 14.30, când începe concursul.
+    </p>
+    <p>
+        Ar trebui să nu fie probleme de conectare (a existat anterior sesiunea de antrenament și etapa județeană), dar pentru situații excepționale puteți folosi totuși adresa de email 
+        <a href="mailto:conturi.osepi8@gmail.com">conturi.osepi8@gmail.com</a>.
+    </p>
+    <p>
+        Ca și la etapa județeană, punctajul la o problemă este cel mai mare dintre toate submisiile de la acea problemă.
+    </p>
+</div>
+<br />
+<div>
+    <b>Pentru clasa a 9-a</b>
+    <p>
+        Proba se va desfășura miercuri 7 aprilie începând cu ora 14.30, va dura 4 ore și va consta din trei probleme de natură algoritmică.
+    </p>
+    <p>
+        Modul de desfășurare este asemănător cu acela de la etapa județeană.
+    </p>
+    <p>
+        Vă rugăm să trimiteți filmările completând următorul <a target="_blank" style="font-weight:bold" href="https://forms.gle/6acufHahcBaj9zdP7">formular</a>. 
+        Trebuie să completați formularul pentru fiecare filmare.
+    </p>
+    <p>
+        Pentru proba de la etapa națională OSEPI clasa a 9-a trebuie să intrați pe linkul <a target="_blank" style="font-weight:bold" href="https://drive.google.com/drive/folders/1F4EIXi0LJ2OuHk4SmkttCRhWjFIBopA1?usp=sharing">acesta</a> 
+        unde veți găsi toate cele necesare (citiți mai întâi documentul <i>Instrucțiuni</i>).
+    </p>
+    <p>
+        <b>Atenție!</b> Enunțurile vor fi disponibile doar prin intermediul linkului de mai sus, nu și la secțiunea <i>Statement</i> a interfeței de concurent.
+    </p>
+    <p>
+        Este esențial să porniți filmarea înainte de ora 14.30, când începe concursul.
+    </p>
+    <p>
+        Ar trebui să nu fie probleme de conectare (a existat anterior sesiunea de antrenament și etapa județeană), 
+        dar pentru situații excepționale puteți folosi totuși adresa de email <a href="mailto:conturi.osepi9@gmail.com">conturi.osepi9@gmail.com</a>. 
+    </p>
+    <p>
+        La Etapa Națională testele vor fi grupate pe subtask-uri. 
+    </p>
+    <p>
+        Scorul unui subtask pentru o singură submisie este minimul scorurilor acordate testelor din subtask. Scorul unui subtask va fi scorul maxim obținut din toate submisiile. 
+        Scorul unei probleme va fi suma scorurilor din toate subtask-urile. (Modelul aplicat la IOI începând din 2017).
+    </p>
+</div>
+<br />
+<div>
+    <b>Pentru clasa a 10-a</b>
+    <p>
+        Proba se va desfășura miercuri 7 aprilie începând cu ora 14.30, va dura 4 ore și va consta din trei probleme de natură algoritmică.
+    </p>
+    <p>
+        Modul de desfășurare este asemănător cu acela de la etapa județeană.
+    </p>
+    <p>
+        Vă rugăm să trimiteți filmările completând următorul <a target="_blank" style="font-weight:bold" href="https://forms.gle/T1ZHfpJTJqrzixwS7">formular</a>. 
+        Trebuie să completați formularul pentru fiecare filmare.
+    </p>
+    <p>
+        Pentru proba de la etapa națională OSEPI clasa a 10-a trebuie să intrați pe linkul <a target="_blank" style="font-weight:bold" href="https://drive.google.com/drive/folders/1gD95h6rN1hiz91vGg9RsV6w4-E7xkwDy?usp=sharing">acesta</a> 
+        unde veți găsi toate cele necesare (citiți mai întâi documentul Instrucțiuni).
+    </p>
+    <p>
+        <b>Atenție!</b> Enunțurile vor fi disponibile doar prin intermediul linkului de mai sus, nu și la secțiunea <i>Statement</i> a interfeței de concurent.
+    </p>
+    <p>
+        Este esențial să porniți filmarea înainte de ora 14.30, când începe concursul.
+    </p>
+    <p>
+        Ar trebui să nu fie probleme de conectare (a existat anterior sesiunea de antrenament și etapa județeană), 
+        dar pentru situații excepționale puteți folosi totuși adresa de email <a href="mailto:conturi.osepi10@gmail.com">conturi.osepi10@gmail.com"</a>. 
+    </p>
+<p>
+    La Etapa Națională testele vor fi grupate pe subtask-uri. 
+</p>
+<p>
+    Scorul unui subtask pentru o singură submisie este minimul scorurilor acordate testelor din subtask. Scorul unui subtask va fi scorul maxim obținut din toate submisiile. 
+    Scorul unei probleme va fi suma scorurilor din toate subtask-urile. (Modelul aplicat la IOI începând din 2017).
+</p>
+</div>
+<br />
+<div>
+<b>Pentru clasele a 11-a și a 12-a</b>
+<p>
+    Proba se va desfășura miercuri 7 aprilie începând cu ora 8.30, va dura 4 ore și va consta din trei probleme de natură algoritmică.
+</p>
+<p>
+    Modul de desfășurare este asemănător cu acela de la etapa județeană.
+</p>
+<p>
+    Vă rugăm să trimiteți filmările completând următorul <a target="_blank" style="font-weight:bold" href="https://forms.gle/PFrpQbAnVFQYHJTu7">formular</a>. 
+    Trebuie să completați formularul pentru fiecare filmare.
+</p>
+<p>
+    Pentru proba de la etapa națională OSEPI clasele a 11-a și a 12-a trebuie să intrați pe linkul <a target="_blank" style="font-weight:bold" href="https://drive.google.com/drive/folders/1H6lyw5au5AufZfzeVKkfcWwmxUL5AB-G?usp=sharing">acesta</a> 
+    unde veți găsi toate cele necesare (citiți mai întâi documentul Instrucțiuni).
+</p>
+<p>
+    <b>Atenție!</b> Enunțurile vor fi disponibile doar prin intermediul linkului de mai sus, nu și la secțiunea <i>Statement</i> a interfeței de concurent.
+</p>
+<p>
+    Este esențial să porniți filmarea înainte de ora 8.30, când începe concursul.
+</p>
+<p>
+    Ar trebui să nu fie probleme de conectare (a existat anterior sesiunea de antrenament și etapa județeană), 
+    dar pentru situații excepționale puteți folosi totuși adresa de email <a href="mailto:conturi.osepi1112@gmail.com">conturi.osepi1112@gmail.com</a>. 
+</p>
+<p>
+    La Etapa Națională testele vor fi grupate pe subtask-uri. 
+</p>
+<p>
+    Scorul unui subtask pentru o singură submisie este minimul scorurilor acordate testelor din subtask. Scorul unui subtask va fi scorul maxim obținut din toate submisiile. 
+    Scorul unei probleme va fi suma scorurilor din toate subtask-urile. (Modelul aplicat la IOI începând din 2017).
+</p>
+</div>
+<br />
+<div style="font-weight:bold">
+    <p>
+        Pentru formarea lotului național de seniori se vor organiza două baraje de selecție în zilele de 9 și 11 aprilie. 
+        Pot participa toți elevii de liceu calificați la etapa națională.
+    </p>
+    <p>
+        Pentru formarea lotului național de juniori se va organiza un baraj de selecție în ziua de 11 aprilie. 
+        Pot participa toți elevii de gimnaziu calificați la etapa națională.
+    </p>
+    <p>
+        La niciuna dintre secțiuni nu se folosesc coeficienți proveniți din probele pe clase.
+    </p>
+    <p>
+        Conform regulamentului, în mod excepțional, un elev de gimnaziu poate solicita participarea la selecția lotului de seniori, dar în acest fel el nu va mai participa la selecția lotului național de juniori. 
+        Acest lucru trebuie comunicat până miercuri 7 aprilie prin email la adresa <a href="mailto:contact@sepi.ro">contact@sepi.ro</a>. 
+        Adresa de pe care se face solicitarea trebuie să fie cea de pe care concurentul s-a înscris.
+    </p>
+</div>
