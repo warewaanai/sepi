977edf683e6aad5267d37607799ddfe77069f3ed
Bodo171 <pbppbp17@gmail.com>
2021-04-06 18:42:57 +0300

    Add 9i

---
+++ srsi_back/static/announcements/9i/content.jsx
@@ -0,0 +1,19 @@
+
+
+<p>
+    Dragi concurenți,
+</p>
+
+<div style="color:blue; font-weight:bold">
+    <p>
+        Având în vedere că după proba de olimpiadă sunt aproape și probele de baraj și astfel aveți nevoie de mult spațiu de stocare în cloud, pentru rundele etapei naționale OSEPI și pentru cele de baraj, acceptăm și varianta înglobării filmării laterale în captura de ecran.
+    </p>
+
+    <p>
+        Atenție, nu este vorba de filmare din față ci de cea laterală care să surprindă mâinile, parte din față și ecranul. De exemplu, acest lucru e posibil prin conectarea unei camere la calculatorul pe care lucrați și plasarea ei corespunzător în lateral, integrând filmarea în captura de ecran cu ajutorul OBS Studio.
+    </p>
+
+    <p>
+        Trebuie să vă asigurați ca ecranul din filmarea laterală (fiind acum mult micșorat), să rămânâ în continuare suficient de clar pentru a se putea confrunta alături de captura de ecran.
+    </p>
+</div>
