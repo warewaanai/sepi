c275698cece3bd5bdc7f8c94e378db58abc40cd1
Bodo171 <pbppbp17@gmail.com>
2021-04-08 17:20:37 +0300

    Add announcement

---
+++ srsi_back/static/announcements/9j/content.jsx
@@ -0,0 +1,156 @@
+<p>
+    Dragi concurenți,
+</p>
+
+<p>
+    Vineri 9 aprilie are loc primul baraj de selecție a lotului național de seniori iar duminică 11 aprilie are loc barajul de selecție a lotului național de juniori precum și al doilea baraj de selecție a lotului național de seniori.
+</p>
+
+<p>
+    La barajele pentru selecția lotului național de seniori pot participa toți elevii calificați la etapa națională la clasele de liceu iar la barajul pentru selecția lotului național de juniori pot participa toți elevii calificați la etapa națională la clasele de gimnaziu.
+</p>
+
+<p>
+    La stabilirea ierarhiei finale nu se folosesc coeficienți proveniți din probele pe clase.
+</p>
+
+<p>
+    Regulile pentru filmare sunt cele de la etapa națională iar modalitatea de trimitere a filmărilor este aceeași.
+</p>
+
+<p>
+    Pentru trimiterea filmărilor vom folosi un formular în care veți indica linkul (linkurile) la care acestea pot fi vizualizate. 
+    Vă rugăm așadar să nu puneți în formular arhive pe care să le descărcăm ci linkuri la care să se poată vizualiza direct. 
+    Formularul are două secțiuni, prima adresată elevilor care au susținut proba de acasă - online individual, iar a doua corespunzătoare celor care au susținut proba în școală sub supravegherea profesorului - online instituțional. 
+    <i>Doar una dintre ele se va completa (pentru câmpurile din cealaltă secțiune completați cu -) </i>și se va trimite. 
+</p>
+<p style="color:blue">
+    Pentru stocarea produselor video puteți utiliza variante diverse de stocare în cloud. De exemplu, diverse conturi google drive, încărcare nelistat pe youtube etc.
+</p>
+<p>
+    Vă rugăm să lucrați relaxat dar totodată să aveți în timpul probei o atitudine care să nu trezească suspiciuni. 
+    Filmarea trebuie să fie audio-video. Nu este permisă folosirea căștilor.
+</p>
+<p>
+    Înainte de prezentarea detaliilor concrete pentru fiecare secțiune în parte, vă aducem la cunoștință câteva lucruri general valabile:
+</p>
+<ul>
+    <li>
+        Odată conectați, aveți acces de pe interfața de concurent la documentație C++.
+    </li>
+    <li>
+        Beneficiați de secțiunea întrebări (Communication/Comunicarea cu juriul) pe interfață până la finalul concursului.
+    </li>
+    <li>
+        Urmăriți secțiunea “Communication/Comunicarea cu juriul” și pentru a vedea eventualele anunțuri generale din timpul probei.
+    </li>
+    <li>
+        La secțiunea “Statement” din interfața de concurent găsiți restricțiile pentru fiecare problemă (Timpul de rulare pe test și Memoria).
+    </li>
+    <li>
+        Reamintim că în timpul probei de concurs sunt permise până la 30 de submisii la o problemă. 
+        Intervalul de timp dintre două submisii trebuie să fie de cel puțin un minut.
+    </li>
+</ul>
+
+<div>
+    <b>
+        Pentru barajul de la gimnaziu
+    </b>
+    <p>
+        Proba se va desfășura duminică 11 aprilie începând cu ora 9.30, va dura 4 ore și va consta din trei probleme de natură algoritmică.
+    </p>
+    <p>
+        Modul de desfășurare este asemănător cu acela de la etapa națională.
+    </p>
+    <p>
+        Vă rugăm să trimiteți filmările completând următorul <b>formular</b> (linkul va apărea în curând).
+    </p>
+    <p>
+        Pentru proba de baraj juniori trebuie să intrați pe linkul <a href="https://drive.google.com/drive/folders/1rUN5ZCStbTFKrPsAUJ7X-83KJ381vy2D?usp=sharing" target="_blank" style="font-weight:bold">acesta</a> 
+        unde veți găsi toate cele necesare (citiți mai întâi documentul <i>Instrucțiuni</i>).
+    </p>
+    <p>
+        <b>Atenție!</b> Enunțurile vor fi disponibile doar prin intermediul linkului de mai sus, nu și la secțiunea Statement a interfeței de concurent.
+    </p>
+    <p>
+        Este esențial să porniți filmarea înainte de ora 9.30, când începe concursul.
+    </p>
+    <p>
+        Ar trebui să nu fie probleme de conectare, dar pentru situații excepționale puteți folosi totuși adresa de email 
+        <a href="mailto:contact@sepi.ro">contact@sepi.ro</a>.
+    </p>
+    <p>
+        Ca și la etapa națională, punctajul la o problemă este cel mai mare dintre toate submisiile de la acea problemă.
+    </p>
+</div>
+
+<div>
+    <b>Pentru Barajul 1 de seniori</b>
+    <p>
+        Proba se va desfășura vineri 9 aprilie începând cu ora 9.30, va dura 5 ore și va consta din trei probleme de natură algoritmică.
+    </p>
+    <p>
+        Modul de desfășurare este asemănător cu acela de la etapa națională.
+    </p>
+    <p>
+        Vă rugăm să trimiteți filmările completând următorul <b>formular</b> (linkul va apărea în curând).
+    </p>
+    <p>
+        Pentru proba de la primul baraj al seniorilor trebuie să intrați pe linkul <a href="https://drive.google.com/drive/folders/1r8xsQ0H4hBhVkuOhdlBmzkiSUm7_1Pcd?usp=sharing" target="_blank" style="font-weight:bold">acesta</a> 
+        unde veți găsi toate cele necesare (citiți mai întâi documentul <i>Instrucțiuni</i>).
+    </p>
+    <p>
+        <b>Atenție!</b> Enunțurile vor fi disponibile doar prin intermediul linkului de mai sus, nu și la secțiunea Statement a interfeței de concurent.
+    </p>
+    <p>
+        Este esențial să porniți filmarea înainte de ora 9.30, când începe concursul.
+    </p>
+    <p>
+        Ar trebui să nu fie probleme de conectare, dar pentru situații excepționale puteți folosi totuși adresa de email 
+        <a href="mailto:contact@sepi.ro">contact@sepi.ro</a>. 
+    </p>
+    <p>
+        Testele pot fi grupate pe subtask-uri. 
+    </p>
+    <p>
+        Scorul unui subtask pentru o singură submisie este minimul scorurilor acordate testelor din subtask. Scorul unui subtask va fi scorul maxim obținut din toate submisiile. 
+        Scorul unei probleme va fi suma scorurilor din toate subtask-urile. (Modelul aplicat la IOI începând din 2017).
+    </p>
+</div>
+
+<div>
+    <b>
+        Pentru Barajul 2 de seniori
+    </b>
+    <p>
+        Proba se va desfășura duminică 11 aprilie începând cu ora 9.30, va dura 5 ore și va consta din trei probleme de natură algoritmică.
+    </p>
+    <p>
+        Modul de desfășurare este asemănător cu acela de la etapa națională.
+    </p>
+    <p>
+        Vă rugăm să trimiteți filmările completând următorul <b>formular</b> (linkul va apărea în curând).
+    </p>
+    <p>
+        Pentru proba de la primul baraj al seniorilor trebuie să intrați pe linkul <a href="https://drive.google.com/drive/folders/1CeQHWkZTvQgLqB_TI_S1GwgshvEJ961B?usp=sharing" target="_blank" style="font-weight:bold">acesta</a> 
+        unde veți găsi toate cele necesare (citiți mai întâi documentul <i>Instrucțiuni</i>).
+    </p>
+    <p>
+        <b>Atenție!</b> Enunțurile vor fi disponibile doar prin intermediul linkului de mai sus, nu și la secțiunea Statement a interfeței de concurent.
+    </p>
+    <p>
+        Este esențial să porniți filmarea înainte de ora 9.30, când începe concursul.
+    </p>
+    <p>
+        Ar trebui să nu fie probleme de conectare, dar pentru situații excepționale puteți folosi totuși adresa de email 
+        <a href="mailto:contact@sepi.ro">contact@sepi.ro</a>. 
+    </p>
+    <p>
+        Testele pot fi grupate pe subtask-uri. 
+    </p>
+    <p>
+        Scorul unui subtask pentru o singură submisie este minimul scorurilor acordate testelor din subtask. Scorul unui subtask va fi scorul maxim obținut din toate submisiile. 
+        Scorul unei probleme va fi suma scorurilor din toate subtask-urile. (Modelul aplicat la IOI începând din 2017).
+    </p>
+</div>
