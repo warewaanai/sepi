62c21bd2deec0f3c0b701a160fe80f9503666d1b
Bodo171 <pbppbp17@gmail.com>
2021-04-11 15:04:27 +0300

    Add announcement

---
+++ srsi_back/static/announcements/9l/content.jsx
@@ -0,0 +1,7 @@
+<p>
+    <b>
+Vom prelungi concursul pana la ora 15.45. In acest timp veti putea submita pe interfata, insa nu garantam feedback-ul. 
+Vom cumula clasamentul de la ora 13.42 (ultimul salvat), cu punctajele obtinute in continuare pe noua interfata. 
+Va rugam sa pastrati filmarile porinite pana la ultima submisie (daca le-ati oprit va rugam sa le reluati).
+</b>
+</p>
