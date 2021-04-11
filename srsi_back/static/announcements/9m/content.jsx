b9a1031829f51d1df98c0dcd06aa1ef938a30e63
Bodo171 <pbppbp17@gmail.com>
2021-04-11 17:11:25 +0300

    Add results

---
+++ srsi_back/static/announcements/9m/content.jsx
@@ -0,0 +1,35 @@
+<p>Dragi concurenți</p>
+
+<p>
+
+    Azi ne-am confruntat cu probleme deosebite în timpul probei de baraj seniori.   
+    În jurul orei 13:50, după 4 ore și 20 de minute de probă (și cu 40 de minute înainte de final) serverul a picat și nu a mai fost posibilă trimiterea de surse pe acel server. 
+    Există clasamentul corespunzător circa orei 13:40.
+</p>
+
+<p>
+    Ulterior s-a pus la dispoziția concurenților un alt server pentru a trimite sursele realizate în ultimul moment. 
+    Astfel a fost posibilă și realizarea unui al doilea clasament la finalul probei, prin cumularea rezultatelor din clasamentul salvat intermediar cu acelea obținute din sursele trimise pe cel de-al doilea server. Punctajul la fiecare problemă a fost calculat drept punctajul maxim la acea problemă dintre punctajul de la ora 13:42 și cel obținut în urma surselor trimise pe al doilea server.
+</p>
+
+<p>
+    Astfel, SEPI a luat următoarea decizie legată de componența lotului:
+</p>
+
+<ol>
+    <li>Se vor cumula clasamentul primei zile de baraj și clasamentul salvat intermediar, iar primii 30 de elevi clasați se califică în lotul național de seniori.</li>
+    <li>Se vor cumula clasamentul primei zile de baraj și clasamentul de la finalul probei, iar primii 30 de elevi clasați se califică în lotul național de seniori.</li>
+</ol>
+
+<p>
+În concluzie, în lotul național de seniori se vor califica elevii clasați între primii 30 în cel puțin unul dintre cele două clasamente (acestea sunt înainte de verificarea video):
+</p>
+
+<ol>
+    <li>
+        <a target="_blank" href={`${window.HOST}/static/lot1.pdf`}>Clasament 1</a>
+    </li>
+    <li>
+        <a target="_blank" href={`${window.HOST}/static/lot2.pdf`}>Clasament 2</a>
+    </li>
+</ol>
