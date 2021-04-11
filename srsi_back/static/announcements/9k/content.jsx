861d275dc874b5f6045b60343701af4fe32d9c76
Bodo171 <pbppbp17@gmail.com>
2021-04-11 14:45:45 +0300

    Add announcement

---
+++ srsi_back/static/announcements/9k/content.jsx
@@ -0,0 +1,8 @@
+<p>Dragi concurenți,</p>
+
+<p>
+Vă rugăm să rămâneți câteva minute lânga calculator cu filmarea pornită întrucât vă vom comunica foarte curând modul în care vom finaliza proba.
+</p>
+<p>
+Este o situație deosebită și participăm cu toții pentru a o trece ccu bine.
+</p>
