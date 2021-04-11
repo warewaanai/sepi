0feb533f89a57576fbb7b738ecfaf66273a78ac3
Bodo171 <pbppbp17@gmail.com>
2021-04-11 13:48:41 +0300

    Add junior results

--- srsi_front/src/pages/Rezultate/listLevels.js
+++ srsi_front/src/pages/Rezultate/listLevels.js
@@ -3,6 +3,7 @@ const listLevels = (reduced=false) => {
     for(let i = 5; i<=12; i+=1)
         levels.push(i.toString());
     if(!reduced) levels.push("baraj");
+    if(!reduced) levels.push("baraj juniori");
     return levels;
 }
 
