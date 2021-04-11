0cf2f162c6cbfd87d3b4cf96a33384e2e45ba3d7
Bodo171 <pbppbp17@gmail.com>
2021-04-11 14:48:28 +0300

    Add announcement

--- srsi_back/static/announcements/9k/content.jsx
+++ srsi_back/static/announcements/9k/content.jsx
@@ -4,5 +4,5 @@
 Vă rugăm să rămâneți câteva minute lânga calculator cu filmarea pornită întrucât vă vom comunica foarte curând modul în care vom finaliza proba.
 </p>
 <p>
-Este o situație deosebită și participăm cu toții pentru a o trece ccu bine.
+Este o situație deosebită și vă solicităm să participăm cu toții pentru a o trece cu bine.
 </p>
