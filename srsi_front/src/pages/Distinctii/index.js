8bd7baa2a6d6256c23057a7f1284e5de365d3026
Bodo171 <pbppbp17@gmail.com>
2021-04-13 18:01:36 +0300

    Fix pdfs

--- srsi_front/src/pages/Distinctii/index.js
+++ srsi_front/src/pages/Distinctii/index.js
@@ -8,7 +8,7 @@ const DistinctiiPage = props =>{
     return <>
         <h1> <TermBlink blink>Distincții etapa națională OSEPI</TermBlink> </h1>
         <ul>
-            {Object.keys(levelNamesONI).map(cell => (<li><a href ={`${window.HOST}/static/awards/`+ cell + `.pdf`}>{levelNamesONI[cell]}</a></li>))}
+            {Object.keys(levelNamesONI).map(cell => (<li><a href ={`${window.HOST}/static/awards/_`+ cell + `.pdf`}>{levelNamesONI[cell]}</a></li>))}
         </ul>
         <a href= {`${window.HOST}/static/awards/lot_juniori.pdf`}>Calificați lot juniori</a> <br />
         <a href= {`${window.HOST}/static/awards/lot_seniori.pdf`}>Calificați lot seniori</a>
