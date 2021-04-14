b5f722733fd69a29569de74809f495c2ce8af042
Bodo171 <pbppbp17@gmail.com>
2021-04-14 19:46:15 +0300

    Add member

---
+++ srsi_back/static/members/Cristian Frâncu/content.jsx
@@ -0,0 +1,50 @@
+
+<ul>
+    <li>
+    Fan al teoriei calculatoarelor
+    </li>
+    <li>
+        Inginer software din întâmplare
+    </li>
+    <li>
+        Instructor de informatică prin opțiune
+    </li>
+</ul>
+<p>
+    Sunt un informatician fascinat de lumea calculatoarelor. Cred în societatea post muncă și în faptul că
+    acum suntem într-o fază de tranziție. Cred că, cu toții, ar trebui să încercăm să ne purtăm ca și când am
+    trăi în acea societate: să muncim mai puțin și să ne distrăm mai mult. În societatea post muncă, munca
+    trebuie să fie distracție, altfel nu are sens să o faci. De aceea predau: este distracția mea.
+    În prezent sunt implicat în diverse proiecte ce au mai multă sau mai puțină legătură cu informatica.
+</p>
+<p>
+    Selecții din viața mea, în ordine invers cronologică:
+</p>
+<ul>
+    <li>
+        Instructor de informatică la Centrul Educațional Nerdvana, București (2017 - prezent)
+    </li>
+    <li>
+        Propunător de probleme la diverse concursuri și baraje: OJI, ONI, RMI, etc.
+    </li>
+    <li>
+        Pregătitor al lotului de gimnaziu al colegiului Tudor Vianu, București în perioada 2012 - 2016
+    </li>
+    <li>
+        Inginer Software la Google, Mountain View, în perioada 2000 - 2006
+    </li>
+    <li>
+        Asistent de cercetare în programul doctoral al Rutgers University, New Jersey cu interese în Machine
+        Learning, Music Information Retrieval, Artificial Intelligence (salvat în ultima clipă de îndrumătorul de
+        proiect care m-a smuls din lumea academică și m-a adus cu forța la Google)
+    </li>
+    <li>
+        Diplomă de studii aprofundate de la Facultatea de Automatică şi Calculatoare din Institutul Politehnic
+        Bucureşti (supravieţuind cursului de Dispozitive şi Circuite Electronice, care a făcut multe victime)
+    </li>
+    <li>
+        Elev la Liceul de Informatică, unde mi-am descoperit pasiunea pentru calculatoare, devenind olimpic
+        naţional la informatică (o schimbare majoră faţă de anii precedenţi, din gimnaziu, când am eram olimpic
+        naţional la fizică şi matematică)
+    </li>
+</ul>
