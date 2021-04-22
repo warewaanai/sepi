fc29213257ae238d1dd69a38fe3acb0e2dd5a7b5
Bodo171 <pbppbp17@gmail.com>
2021-04-22 22:18:10 +0300

    Add member

--- srsi_front/src/pages/Despre/parteneri.js
+++ srsi_front/src/pages/Despre/parteneri.js
@@ -18,6 +18,8 @@ import univiasi from './univiasi.jpg'
 import bolyai from './babes_bolyai.gif'
 import revista_infobits from './revista_infobits.png'
 import infobits_academy from './infobits_academy.png'
+import upit from './upit.png'
+
 
 const PartenerCard = props => {
     return (
@@ -35,7 +37,7 @@ const Parteneri = props => {
 
         <Carousel content={[
             [{link: "https://pbinfo.ro/", pic: Pbinfo}, {link: "http://upir.ro/index.php/ro/", pic: upir}, {link: "https://profs.info.uaic.ro/~consortiu/index.html", pic: universitaria}, {link: "https://www.infobits.ro/", pic: infobits_academy}],
-            [{link: "https://infoarena.ro/", pic: Infoarena}, {link: "https://www.info.uaic.ro/", pic: FII}, {link: "https://www.uaic.ro/", pic: univiasi}, {link: "https://liceecentenare.ro/", pic: ACC}],
+            [{link: "https://infoarena.ro/", pic: Infoarena}, {link: "https://www.info.uaic.ro/", pic: FII}, {link: "https://www.uaic.ro/", pic: univiasi}, {link: "https://liceecentenare.ro/", pic: ACC}, {link: "https://www.upit.ro/ro/academia-reorganizata/facultatea-de-stiinte-educatie-fizica-si-informatica", pic: upit}],
             [{link: "https://rms.unibuc.ro/", pic: SSMR}, {link: "https://www.uvt.ro/ro/", pic: UVT}, {link: "https://unibuc.ro/", pic: unibuc}, {link: "https://www.ubbcluj.ro/ro/", pic: bolyai}, {link: "https://revista.infobits.ro/", pic: revista_infobits}]
         ]}/>
     </div>
