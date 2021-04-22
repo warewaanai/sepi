89ba3d09bd858ef368ec7e16aa909f7b9d907a28
Bodo171 <pbppbp17@gmail.com>
2021-04-22 21:34:15 +0300

    Balliol

---
+++ srsi_back/static/announcements/9o/content.jsx
@@ -0,0 +1,43 @@
+<p>
+    Dragi concurenți,
+</p>
+
+
+<p>
+    De mâine și până miercuri avem un nou maraton de programare competițională: barajele pentru selectarea echipelor reprezentative: trei probe pentru lotul de juniori și trei probe pentru lotul de seniori.
+</p>
+<p>
+    Toți elevii vor susține probele pe scenariul online instituțional.
+</p>
+<p>
+    Produsele video se vor trimite până la finalul zilei de concurs prin intermediul unor formulare, în mod similar cu acela folosit la etapa națională: 
+    pentru juniori <a href="https://forms.gle/zurR6956RhGLaDuFA" target="_blank" style="font-weight:bold">aici</a>, pentru seniori <a href="https://forms.gle/cJ6JxrCYDPuKm6jT6" target="_blank" style="font-weight:bold">aici</a>
+    (se va folosi același formular pentru fiecare dintre cele trei baraje).
+</p>
+
+<p>
+    Programul este următorul: <br />
+    Baraj 1 juniori: vineri 23 aprilie începând cu ora 9 pe durata a 4 ore. <br />
+    Baraj 1 seniori: duminică 25 aprilie începând cu ora 9 pe durata a 5 ore. <br />
+    Baraj 2 juniori: luni 26 aprilie începând cu ora 9 pe durata a 4 ore. <br />
+    Baraj 2 seniori: luni 26 aprilie începând cu ora 9 pe durata a 5 ore. <br />
+    Baraj 3 juniori: miercuri 28 aprilie începând cu ora 9 pe durata a 4 ore. <br />
+    Baraj 3 seniori: miercuri 28 aprilie începând cu ora 9 pe durata a 5 ore. <br />
+</p>
+
+
+<p>
+    Probele constau din câte 3 probleme de natură algoritmică. <br />
+    Concurenții se pot conecta începând cu ora 8.30. <br />
+    Modul de desfășurare este asemănător cu acela de la etapa națională. <br />
+    Concurenții vor primi linkul de acces la interfața de concurs pe adresa de email folosită la înscriere. <br />
+    Subiectele vor fi disponibile pe interfața de concurs la ora începerii probei. <br />
+    Modalitatea de punctare (grupare sau nu a testelor etc.) va fi comunicată pentru fiecare problemă în parte. <br />
+</p>
+
+<p>
+    După fiecare probă vor fi publicate enunțurile, descrierile de soluții, testele de la evaluare și rezultatele la  
+    <a href="https://drive.google.com/drive/folders/1NCY8yKefLdePIQ9LSfDWw8yJwKcuVkDf?usp=sharing" target="_blank" style="font-weight:bold">acest link</a>.
+</p>
+
+Pentru lămuriri suplimentare puteți folosi adresa de email <a href="mailto:contact@sepi.ro">contact@sepi.ro</a>.
