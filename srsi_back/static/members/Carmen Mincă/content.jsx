c33631db55ddafb6b846683739a9bdb7b14d18ea
Bodo171 <pbppbp17@gmail.com>
2021-05-04 16:22:58 +0300

    Add member

---
+++ srsi_back/static/members/Carmen Mincă/content.jsx
@@ -0,0 +1,29 @@
+<ul>
+<li>
+    Profesor la Colegiul National de Informatică “Tudor Vianu”, București
+</li>
+<li>
+    Distincția ”Gheorghe Lazăr” clasa a III-a (2007)
+</li>
+<li>
+    Membru in Comisia Națională de Specialitate, disciplina Informatică a Ministerului Educației Naționale (2010-până în prezent)
+</li>
+<li>
+    Membru în Comisia Națională de elaborare subiecte și evaluare a Olimpiadei Naționale de Informatică, edițiile (2006-2021)
+</li>
+<li>
+    Vicepreședinte subcomisii în cadrul Olimpiadei Naționale de Informatică
+</li>
+<li>
+    Membru al grupului de lucru pentru elaborare subiecte examen bacalaureat informatică (2006-2016)
+</li>
+<li>
+    Membru în comisia de elaborare subiecte si evaluare a Concursului Național de informatică "Grigore Moisil", Lugoj (edițiile 2007, 2008, 2011-2016)
+</li>
+<li>
+    Membru în comisia de organizare al Concursului International RMI   (Romanian Master of Informatics) (2014-până în prezent)
+</li>
+<li>
+    Autor/coautor a peste 30 de publicații – Manuale,cărți de specialitate, ghiduri
+</li>
+</ul>
