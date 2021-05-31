36446db78425fe0860b574223d8fd79550a5d2d9
Bodo171 <pbppbp17@gmail.com>
2021-05-31 17:42:13 +0300

    Add nerdvana

--- srsi_front/src/pages/Despre/parteneri.js
+++ srsi_front/src/pages/Despre/parteneri.js
@@ -19,7 +19,7 @@ import bolyai from './babes_bolyai.gif'
 import revista_infobits from './revista_infobits.png'
 import infobits_academy from './infobits_academy.png'
 import upit from './upit.png'
-
+import nerdvana from './nerdvana.png';
 
 const PartenerCard = props => {
     return (
@@ -36,7 +36,7 @@ const Parteneri = props => {
         <h1><TermBlink>Partenerii noștri</TermBlink></h1>
 
         <Carousel content={[
-            [{link: "https://pbinfo.ro/", pic: Pbinfo}, {link: "http://upir.ro/index.php/ro/", pic: upir}, {link: "https://profs.info.uaic.ro/~consortiu/index.html", pic: universitaria}, {link: "https://www.infobits.ro/", pic: infobits_academy}],
+            [{link: "https://pbinfo.ro/", pic: Pbinfo}, {link: "http://upir.ro/index.php/ro/", pic: upir}, {link: "https://profs.info.uaic.ro/~consortiu/index.html", pic: universitaria}, {link: "https://www.infobits.ro/", pic: infobits_academy}, {link: "https://nerdvana.ro/", pic: nerdvana}],
             [{link: "https://infoarena.ro/", pic: Infoarena}, {link: "https://www.info.uaic.ro/", pic: FII}, {link: "https://www.uaic.ro/", pic: univiasi}, {link: "https://liceecentenare.ro/", pic: ACC}, {link: "https://www.upit.ro/ro/academia-reorganizata/facultatea-de-stiinte-educatie-fizica-si-informatica", pic: upit}],
             [{link: "https://rms.unibuc.ro/", pic: SSMR}, {link: "https://www.uvt.ro/ro/", pic: UVT}, {link: "https://unibuc.ro/", pic: unibuc}, {link: "https://www.ubbcluj.ro/ro/", pic: bolyai}, {link: "https://revista.infobits.ro/", pic: revista_infobits}]
         ]}/>
